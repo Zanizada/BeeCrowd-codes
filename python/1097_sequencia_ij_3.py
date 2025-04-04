b25ef79a71c3ffe1bd0f04689a53aa8938d60db2
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-04-04 16:36:47 -0300

    Create 1097_sequencia_ij_3.py

---
+++ python/1097_sequencia_ij_3.py
@@ -0,0 +1,16 @@
+I = 1
+J = 7
+contador = 0
+
+while I <= 9:
+    print(f'I={I} J={J}')
+    J -= 1
+    contador += 1
+
+    if contador == 3:
+        I += 2
+        J += 5
+        contador = 0
+
+    if I > 9:
+        break
