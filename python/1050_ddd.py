121e6a959f71bda4dd6ef850fa36ec936bab010f
Zanizada <rafaelgizzi@gmail.com>
2025-04-04 00:14:08 -0300

    Create 1050_ddd.py

---
+++ python/1050_ddd.py
@@ -0,0 +1,20 @@
+DDD = int(input())
+
+if DDD == 61:
+    print(f'Brasilia')
+elif DDD == 71:
+    print(f'Salvador')
+elif DDD == 11:
+    print(f'Sao Paulo')
+elif DDD == 21:
+    print(f'Rio de Janeiro')
+elif DDD == 32:
+    print(f'Juiz de Fora')
+elif DDD == 19:
+    print(f'Campinas')
+elif DDD == 27:
+    print(f'Vitoria')
+elif DDD == 31:
+    print(f'Belo Horizonte')
+else:
+    print(f'DDD nao cadastrado')
