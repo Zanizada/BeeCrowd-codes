0e10fd8d41c7ff6698de69c5e0de0e78c87562e9
Zanizada <rafaelgizzi@gmail.com>
2025-04-04 00:09:39 -0300

    Create 1045_tipos_de_triangulos.py

---
+++ python/1045_tipos_de_triangulos.py
@@ -0,0 +1,18 @@
+A, B, C = map(float, input().split())
+
+triangulo = sorted([A, B, C], reverse=True)
+A, B, C = triangulo
+
+if A >= B + C:
+    print("NAO FORMA TRIANGULO")
+else:
+    if A**2 == B**2 + C**2:
+        print("TRIANGULO RETANGULO")
+    elif A**2 > B**2 + C**2:
+        print("TRIANGULO OBTUSANGULO")
+    elif A**2 < B**2 + C**2:
+        print("TRIANGULO ACUTANGULO")
+    if A == B == C:
+        print("TRIANGULO EQUILATERO")
+    elif A == B or B == C or A == C:
+        print("TRIANGULO ISOSCELES")
