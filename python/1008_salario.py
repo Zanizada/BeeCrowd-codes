16004418126add54e88e202b019fff0477a9924e
Zanizada <rafaelgizzi@gmail.com>
2025-04-03 23:34:29 -0300

    Create 1008_salario.py

---
+++ python/1008_salario.py
@@ -0,0 +1,6 @@
+NUMBER = int(input())    # NUMBER = Número do funcionário
+Nht = int(input())   # Nht = Número de horas trabalhadas
+Vph = float(input()) # Vph = Valor por hora
+SALARY = Nht * Vph
+print(f'NUMBER = {NUMBER}')
+print(f'SALARY = U$ {SALARY:.2f}')
