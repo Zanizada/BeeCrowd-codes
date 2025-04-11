a7d6548cd31e27250f0a70a2ec1dac4b3e23d50e
Zanizada <rafaelgizzi@gmail.com>
2025-04-11 20:30:01 -0300

    New item listed in local repository

--- java/java/java/extremamente_basico_1001.java
+++ java/java/java/extremamente_basico_1001.java
@@ -1,33 +1,16 @@
+import java.util.Scanner;
 
-/**
- * Escreva uma descrição da classe extremamente_basico_1001 aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
- */
-public class extremamente_basico_1001
-{
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
-
-    /**
-     * Construtor para objetos da classe extremamente_basico_1001
-     */
-    public extremamente_basico_1001()
-    {
-        // inicializa variáveis de instância
-        x = 0;
-    }
-
-    /**
-     * Um exemplo de um método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   um exemplo de um parâmetro de método
-     * @return     a soma de x e y 
-     */
-    public int sampleMethod(int y)
-    {
-        // escreva seu código aqui
-        return x + y;
+public class extremamente_basico_1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        int X = A + B;
+        
+        System.out.println("X = " + X);
+        
+        sc.close();
     }
 }
