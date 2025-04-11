6a4d25948414fd092bddaf7ef0fb5f5d099d0a02
Zanizada <rafaelgizzi@gmail.com>
2025-04-11 20:00:02 -0300

    New item listed in local repository

---
+++ java/java/java/extremamente_basico_1001.java
@@ -0,0 +1,33 @@
+
+/**
+ * Escreva uma descrição da classe extremamente_basico_1001 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class extremamente_basico_1001
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * Construtor para objetos da classe extremamente_basico_1001
+     */
+    public extremamente_basico_1001()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    public int sampleMethod(int y)
+    {
+        // escreva seu código aqui
+        return x + y;
+    }
+}
