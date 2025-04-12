221a516b7d2b9ffeb0e85cbaeccc31a689a67c35
Zanizada <rafaelgizzi@gmail.com>
2025-04-11 22:00:02 -0300

    New item listed in local repository

--- java/java/hello_world_1000.java
+++ java/java/hello_world_1000.java
@@ -1,4 +1,4 @@
-public class Hello_World_1000 {
+public class hello_world_1000 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- java/java/soma_simples_1003.java
+++ java/java/soma_simples_1003.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class area_do_circulo_1002 {
+public class soma_simples_1003 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
