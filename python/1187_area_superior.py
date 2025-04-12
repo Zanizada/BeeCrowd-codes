e3735f309bb7c7ed242ac8bf14d7e3f4441d8fa9
Zanizada <rafaelgizzi@gmail.com>
2025-04-12 17:00:02 -0300

    New item listed in local repository

---
+++ python/1187_area_superior.py
@@ -0,0 +1,24 @@
+O = input().strip().upper()
+matriz = []
+
+for i in range(12):
+    linha = []
+    for j in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+soma = 0
+quantidade = 0
+
+for i in range(12):
+    for j in range(i + 1, 12):
+        if j > i and i + j >= 11:
+            soma += matriz[i][j]
+            quantidade += 1
+    
+if O == "S":
+    print(f"{soma:.1f}")
+
+else:
+    media = soma / quantidade
+    print(f"{media:.1f}")
