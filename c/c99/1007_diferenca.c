4c5f960ca9f768f3145e313d0f348971c05cd1f3
Zanizada <rafaelgizzi@gmail.com>
2025-04-12 18:30:01 -0300

    New item listed in local repository

---
+++ c/c99/1007_diferenca.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int A, B, C, D, PA, PB, diff;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+    PA = A * B;
+    PB = C * D;
+    diff = PA - PB;
+    printf("DIFERENCA = %d\n" diff)
+}
