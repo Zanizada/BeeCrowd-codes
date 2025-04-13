7e463ccdc9bb392a1fb2f335f2516abf94c71a84
Zanizada <rafaelgizzi@gmail.com>
2025-04-13 02:00:01 -0300

    File modified in local repository

--- c/c99/1037_intervalo.c
+++ c/c99/1037_intervalo.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    double N;
+    scanf("%lf", &N);
+    if (N >= 0 && N <= 25.0000) {
+        printf("Intervalo [0,25]\n");
+    }
+    else if (N > 25.00001 && N <= 50.0000) {
+        printf("Intervalo (25,50]\n");
+    }
+    else if (N > 50.00001 && N <= 75.0000) {
+        printf("Intervalo (50,75]\n");
+    }
+    else if (N > 75.00001 && N <= 100.0000) {
+        printf("Intervalo (75,100]\n");
+    }
+    else {
+        printf("Fora de intervalo\n");
+    }
+    return 0;
+}
