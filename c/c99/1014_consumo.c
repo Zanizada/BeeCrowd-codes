d1c9fbd6e9723100b4d3952cd52e88e5e49f73a5
Zanizada <rafaelgizzi@gmail.com>
2025-04-12 21:30:01 -0300

    New item listed in local repository

--- c/c99/1014_consumo.c
+++ c/c99/1014_consumo.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main() {
+    int X;
+    double Y, total;
+    scanf("%d", &X);
+    scanf("%lf", &Y);
+    total = X / Y;
+    printf("%.3lf km/l\n", total);
+    return 0;
+}
