cc33fac46041dfad16b7bef3da8225718db22aca
Zanizada <rafaelgizzi@gmail.com>
2025-04-13 00:30:01 -0300

    New item listed in local repository

--- c/c99/1021_notas_e_moedas.c
+++ c/c99/1021_notas_e_moedas.c
@@ -5,14 +5,14 @@ int main() {
     int notas[] = {100, 50, 20, 10, 5, 2};
     int moedas[] = {100, 50, 25, 10, 5, 1};
 
-    scanf("%1f", &valor);
+    scanf("%lf", &valor);  // CORRETO para double
 
     int valor_em_centavos = (int)(valor * 100 + 0.5);
 
     printf("NOTAS:\n");
     for (int i = 0; i < 6; i++) {
         int nota = notas[i];
-        int quantidade = valor_em_centavos / (nota *100);
+        int quantidade = valor_em_centavos / (nota * 100);
         printf("%d nota(s) de R$ %d.00\n", quantidade, nota);
         valor_em_centavos %= (nota * 100);
     }
@@ -21,10 +21,7 @@ int main() {
     for (int i = 0; i < 6; i++) {
         int moeda = moedas[i];
         int quantidade = valor_em_centavos / moeda;
-        if (moeda >= 100)
-            printf("%d moeda(s) de R$ 1.00\n", quantidade);
-        else
-            printf("%d moeda(s) de R$ 0.%02d\n", quantidade, moeda);
+        printf("%d moeda(s) de R$ %.2f\n", quantidade, moeda / 100.0);
         valor_em_centavos %= moeda;
     }
 
