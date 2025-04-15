73b2f4a2e4fea7a93a1c4ee8e43a1deaf43f6f65
Zanizada <rafaelgizzi@gmail.com>
2025-04-15 10:02:12 -0300

    New item listed in local repository

--- c/c99/1038_lanche.c
+++ c/c99/1038_lanche.c
@@ -3,27 +3,26 @@
 int main() {
     int N, Q;
     float preco;
-    scanf("%d", &N);
-    scanf("%d", &Q);
-    if N == 1 {
+    scanf("%d %d", &N, &Q);
+    if (N == 1) {
         preco = Q * 4.00;
-        printf("Total: R$%.2f\n", preco);
+        printf("Total: R$ %.2f\n", preco);
     }
-    else if N == 2 {
+    else if (N == 2) {
         preco = Q * 4.50;
-        printf("Total: R$%.2f\n", preco);
+        printf("Total: R$ %.2f\n", preco);
     }
-    else if N == 3 {
+    else if (N == 3) {
         preco = Q * 5.00;
-        printf("Total: R$%.2f\n", preco);
+        printf("Total: R$ %.2f\n", preco);
     }
-    else if N == 4 {
+    else if (N == 4) {
         preco = Q * 2.00;
-        printf("Total: R$%.2f\n", preco);
+        printf("Total: R$ %.2f\n", preco);
     }
-    else if N == 5 {
+    else if (N == 5) {
         preco = Q * 1.50;
-        printf("Total: R$%.2f\n", preco);
+        printf("Total: R$ %.2f\n", preco);
     }
     return 0;
 }
