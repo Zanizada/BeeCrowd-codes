bff3bfb3ecd7cc06cf3fcf3d5d6df1d0aed27795
Zanizada <rafaelgizzi@gmail.com>
2025-04-16 19:20:02 -0300

    File modified in local repository

--- c/c99/1040_media_3.c
+++ c/c99/1040_media_3.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    float N1, N2, N3, N4, media, exame, media_final;
+    scanf("%f %f %f %f", &N1, &N2, &N3, &N4);
+    media = ((N1*2)+(N2*3)+(N3*4)+(N4*1))/(2+3+4+1);
+    printf("Media: %.1f\n", &media);
+    if (media >= 7.0) {
+        printf("Aluno aprovado.\n");
+    }
+    else if (media < 5.0) {
+        printf("Aluno reprovado.\n");
+    } else {
+        printf("Aluno em exame.\n");
+        scanf("%f", &exame);
+        printf("Nota do exame: %.1f\n", exame);
+        media_final = (exame+media)/2;
+        if (media_final >= 5.0) {
+            printf("")
+        }
+    }
+}
