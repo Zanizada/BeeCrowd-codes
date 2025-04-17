9abcde27d8f4794a3e1c7cb8cf0745543489edc0
Zanizada <rafaelgizzi@gmail.com>
2025-04-17 11:00:03 -0300

    File modified in local repository

--- c/c99/1045_tipos_de_triangulos.c
+++ c/c99/1045_tipos_de_triangulos.c
@@ -2,8 +2,13 @@
 #include <math.h>
 
 int main() {
-    double A, B, C;
+    double A, B, C, temp;
     scanf("%lf %lf %lf", &A, &B, &C);
+
+    if (A < B) { temp = A; A = B; B = temp; }
+    if (A < C) { temp = A; A = C; C = temp; }
+    if (B < C) { temp = B; B = C; C = temp; }
+
     if (A >= B + C) {
         printf("NAO FORMA TRIANGULO\n");
     }
@@ -16,10 +21,11 @@ int main() {
     else if (pow(A, 2) < pow(B, 2) + pow(C, 2)) {
         printf("TRIANGULO ACUTANGULO\n");
     }
-    else if (A == B == C) {
+
+    if (A == B && B == C) {
         printf("TRIANGULO EQUILATERO\n");
     }
-    else if (A == B != C || A == C != B || B == C != A) {
+    else if ((A == B && B != C) || (A == C && C == B) || (B == C && C != A)) {
         printf("TRIANGULO ISOSCELES\n");
     }
 }
