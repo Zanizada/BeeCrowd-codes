8b4aafb0985838379ee62ec0909c2c97f203bc4a
Zanizada <rafaelgizzi@gmail.com>
2025-04-17 08:50:02 -0300

    File modified in local repository

--- c/c99/1042_sort_simples.c
+++ c/c99/1042_sort_simples.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int compare(const void *x, const void *y) {
+    return (*(int*)x - *(int*)y);
+}
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int numeros[3] = {a, b, c};
+    qsort(numeros, 3, sizeof(int), compare);
+    for (int i = 0; i < 3; i++) {
+        printf("%d\n", numeros[i]);
+    }
+    printf("\n%d\n%d\n%d\n", a, b, c);
+    return 0;
+}
