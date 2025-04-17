89cdc1d59d7be53b6dbf90ecf6f8a1e40398ef0b
Zanizada <rafaelgizzi@gmail.com>
2025-04-16 21:50:02 -0300

    File modified in local repository

--- c/c99/1040_media_3.c
+++ c/c99/1040_media_3.c
@@ -4,7 +4,7 @@ int main() {
     float N1, N2, N3, N4, media, exame, media_final;
     scanf("%f %f %f %f", &N1, &N2, &N3, &N4);
     media = ((N1*2)+(N2*3)+(N3*4)+(N4*1))/(2+3+4+1);
-    printf("Media: %.1f\n", &media);
+    printf("Media: %.1f\n", media);
     if (media >= 7.0) {
         printf("Aluno aprovado.\n");
     }
@@ -20,6 +20,6 @@ int main() {
         } else {
             printf("Aluno reprovado.\n");
         }
-        printf("Media final: %.1f\n");
+        printf("Media final: %.1f\n", media_final);
     }
 }
