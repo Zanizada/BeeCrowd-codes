4706a2da2d88e1cc14b1c4a23cb70e035b826aca
Zanizada <rafaelgizzi@gmail.com>
2025-04-17 10:50:03 -0300

    File modified in local repository

--- c/c99/1045_tipos_de_triangulos.c
+++ c/c99/1045_tipos_de_triangulos.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    double A, B, C;
+    if (A >= B + C) {
+        printf("NAO FORMA TRIANGULO\n");
+    }
+    else if (pow(A, 2) = pow(B, 2) + pow(C, 2) {
+        printf("TRIANGULO RETANGULO\n");
+    }
+    else if (pow(A, 2) > pow(B, 2) + pow(C, 2) {
+        printf("TRIANGULO OBTUSANGULO\n");
+    }
+    else if (pow(A, 2) < pow(B, 2) + pow(C, 2) {
+        printf("TRIANGULO ACUTANGULO\n");
+    }
+    else if (A == B == C)
+}
