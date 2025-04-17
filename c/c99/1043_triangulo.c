27ebe8c0cdc688feea818ed4db347c42ac983bb5
Zanizada <rafaelgizzi@gmail.com>
2025-04-17 09:10:02 -0300

    New item listed in local repository

--- c/c99/1043_triangulo.c
+++ c/c99/1043_triangulo.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    float A, B, C;
+    scanf("%f %f %f", &A, &B, &C);
+    if (A < B + C && B < A + C && C < A + B) {
+        float perimetro;
+        perimetro = A + B + C;
+        printf("Perimetro = %.1f\n", perimetro);
+    } else {
+        float area;
+        area = ((A + B) * C) / 2;
+        printf("Area = %.1f\n", area);
+    }
+    return 0;
+}
