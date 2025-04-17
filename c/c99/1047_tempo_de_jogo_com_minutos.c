627b80620745f5dfacb94947b4e28b835d157f66
Zanizada <rafaelgizzi@gmail.com>
2025-04-17 12:50:02 -0300

    File modified in local repository

--- c/c99/1047_tempo_de_jogo_com_minutos.c
+++ c/c99/1047_tempo_de_jogo_com_minutos.c
@@ -1,14 +1,1 @@
 #include <stdio.h>
-
-int main() {
-    int h_inicio, h_fim, m_inicio, m_fim, duracao;
-    scanf("%d %d %d %d", &h_inicio, &m_inicio, &h_fim, &m_fim);
-    if (h_inicio < h_fim) {
-        duracao = fim - inicio;
-    } else {
-        duracao = (24 - inicio) + fim;
-    }
-    printf("O JOGO DUROU %d HORA(S)\n", duracao);
-
-    return 0;
-}
