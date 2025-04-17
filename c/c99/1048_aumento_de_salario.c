8a4317073276c79ef95521e4c1d745b8c42a8b0a
Zanizada <rafaelgizzi@gmail.com>
2025-04-17 17:10:02 -0300

    File modified in local repository

--- c/c99/1048_aumento_de_salario.c
+++ c/c99/1048_aumento_de_salario.c
@@ -3,28 +3,29 @@
 int main() {
     float salario, reajuste, ganho;
     int porcentagem;
+    scanf("%f", &salario);
     if (0 <= salario && salario <= 400.00) {
         reajuste = salario * 1.15;
         porcentagem = 15;
-        ganho = salario - reajuste;
+        ganho = reajuste - salario;
     } else if (400.01 <= salario && salario <= 800.00) {
         reajuste = salario * 1.12;
         porcentagem = 12;
-        ganho = salario - reajuste;
+        ganho = reajuste - salario;
     } else if (800.01 <= salario && salario <= 1200.00) {
         reajuste = salario * 1.10;
         porcentagem = 10;
-        ganho = salario - reajuste;
+        ganho = reajuste - salario;
     } else if (1200.01 <= salario && salario <= 2000.00) {
         reajuste = salario * 1.07;
         porcentagem = 7;
-        ganho = salario - reajuste;
+        ganho = reajuste - salario;
     } else {
         reajuste = salario * 1.04;
         porcentagem = 4;
-        ganho = salario - reajuste;
+        ganho = reajuste - salario;
     }
-    printf("Novo Salario: %.2f\nReajuste ganho: %.2f\nEm percentual: %d %\n", reajuste, ganho, porcentagem);
+    printf("Novo Salario: %.2f\nReajuste ganho: %.2f\nEm percentual: %d %%\n", reajuste, ganho, porcentagem);
 
     return 0;
 }
