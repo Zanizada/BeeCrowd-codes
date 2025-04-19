fa188c24260da9ebebfc8e358c28fd4aaa34fc3c
Zanizada <rafaelgizzi@gmail.com>
2025-04-19 20:30:02 -0300

    File modified in local repository

--- c/c99/1051_imposto_de_renda.c
+++ c/c99/1051_imposto_de_renda.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    float salario, imposto;
+    float salario, imposto = 0.0;
     scanf("%f", &salario);
 
+    if (salario <= 2000.00) {
+        printf("Isento\n");
+    } else if (salario <= 3000.00) {
+        imposto = (salario - 2000.00) * 0.08;
+        printf("R$ %.2f\n", imposto);
+    } else if (salario <= 4500.00) {
+        imposto = (1000.00 * 0.08) + (salario - 3000.00) * 0.18;
+        printf("R$ %.2f\n", imposto);
+    } else {
+        imposto = (1000.00 * 0.08) + (1500.00 * 0.18) + (salario - 4500.00) * 0.28;
+        printf("R$ %.2f\n", imposto);
+    }
+
+    return 0;
 }
