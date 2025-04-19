f7c962d75d93d8e8b9b8d4851d5ec5859f198326
Zanizada <rafaelgizzi@gmail.com>
2025-04-19 20:40:02 -0300

    File modified in local repository

--- c/c99/1052_mes.c
+++ c/c99/1052_mes.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    int N;
+    scanf("%d", &N);
+    char meses[12];
+    meses[12] = {"January", "February", "March",
+    "April", "May", "June", "July", "August",
+    "September", "October", "November", "December"
+    }
+}
