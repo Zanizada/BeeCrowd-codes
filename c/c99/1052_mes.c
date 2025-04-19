91d10c07ca315bcb576fed4159b25273d5deadd9
Zanizada <rafaelgizzi@gmail.com>
2025-04-19 20:42:40 -0300

    File modified in local repository

--- c/c99/1052_mes.c
+++ c/c99/1052_mes.c
@@ -3,9 +3,16 @@
 int main() {
     int N;
     scanf("%d", &N);
-    char meses[12];
-    meses[12] = {"January", "February", "March",
-    "April", "May", "June", "July", "August",
-    "September", "October", "November", "December"
+    const char* meses[12] = {
+        "January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"
+    };
+
+    if (N >= 1 && N <= 12) {
+        printf("%s\n", meses[N - 1]);
     }
+
+    return 0;
 }
