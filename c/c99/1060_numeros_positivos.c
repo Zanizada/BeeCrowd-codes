82d6eb36b12b9837f55945cea02f476b4bce377b
Zanizada <rafaelgizzi@gmail.com>
2025-04-19 21:10:02 -0300

    File modified in local repository

--- c/c99/1060_numeros_positivos.c
+++ c/c99/1060_numeros_positivos.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
-
+    int positivos = 0;
+    float N;
+    for (int i = 1; i <= 6; i++) {
+        scanf("%f", &N);
+        if (N > 0) {
+            positivos++;
+        }
+    }
+    printf("%d valores positivos\n", positivos);
+    return 0;
 }
