7392b85cd13bfbed0d6753e78031e323463e312f
Zanizada <rafaelgizzi@gmail.com>
2025-04-20 01:40:02 -0300

    File modified in local repository

--- c/c99/1061_tempo_de_um_evento.c
+++ c/c99/1061_tempo_de_um_evento.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int inicio;
+    scanf("%d", &inicio);
+    int h1, m1, s1;
+    scanf("%d : %d : %d", &h1, &m1, &s1);
+    int fim;
+    scanf("%d", &fim);
+    int h2, m2, s2;
+    scanf("%d : %d : %d", &h2, &m2, &s2);
+    int dias;
+
+}
