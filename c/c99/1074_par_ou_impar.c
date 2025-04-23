1a827b15fd557a272821daf3eb83f98c429d43e8
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 11:26:04 -0300

    File modified in local repository

--- c/c99/1074_par_ou_impar.c
+++ c/c99/1074_par_ou_impar.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int N, A;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &A);
+        if ((A % 2 == 0)&&(A > 0)) {
+
+        }
+    }
+
+    return 0;
+}
