25cd193a3ffaca5c6a749adf23c61c9ea1ccd165
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 14:16:04 -0300

    File modified in local repository

--- c/c99/1078_tabuada.c
+++ c/c99/1078_tabuada.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int N, multiplo = 1;
+    scanf("%d", &N);
+
+    for (int i = 0; i < 10; i++) {
+        tabuada = multiplo * N;
+        printf("%d x %d = %d", multiplo, N, tabuada);
+        multiplo++;
+    }
+
+    return 0;
+}
