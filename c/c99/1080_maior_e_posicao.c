c146055ab8e9af0db29694c7e132e6da0367678e
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 16:20:09 -0300

    File modified in local repository

--- c/c99/1080_maior_e_posicao.c
+++ c/c99/1080_maior_e_posicao.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    int i, numeros[100];
+    for (i = 0; i < 100; i++) {
+        scanf("%d", &numeros[i]);
+    }
+    int maior = numeros[0], menor = numeros[0];
 
+    for (i = 1; i < 100; i++) {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+        }
+        if (numeros[i] < menor) {
+            menor = numeros[i];
+        }
+    }
+
+    printf("%d\n", maior);
+    printf("%d\n", menor);
 
     return 0;
 }
