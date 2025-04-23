3bf336da8877ef2fbb871234e009f36b48776475
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 10:30:02 -0300

    New item listed in local repository

--- c/c99/1070_seis_numeros_impares.c
+++ c/c99/1070_seis_numeros_impares.c
@@ -5,7 +5,11 @@ int main() {
     scanf("%d", &X);
 
     while (contador < 6) {
-        if (X % 2 != 0) printf("%d\n", X);
+        if (X % 2 != 0) {
+            printf("%d\n", X);
+            contador++;
+        }
+        X++;
     }
     return 0;
 }
