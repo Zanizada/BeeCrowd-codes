87be639e896c6bd3b6e2937de724b7d1570f7fd8
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 16:50:02 -0300

    File modified in local repository

--- c/c99/1094_experiencias.c
+++ c/c99/1094_experiencias.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int N, total = 0, coelhos = 0, ratos = 0, sapos = 0, percentual_c = 0, percentual_r = 0, percentual_s = 0;
+    int N, quantia, total = 0, coelhos = 0, ratos = 0, sapos = 0, percentual_c = 0, percentual_r = 0, percentual_s = 0;
     char tipo[50];
     scanf("%d", &N);
 
     for (int i = 0; i < N; i++) {
-        scanf("%d %s", quantia, tipo[50]);
-
+        scanf("%d %s", &quantia, &tipo[50]);
+        total += quantia;
+        if (tipo[50] == "C") {
+            coelhos += quantia;
+        } else if (tipo[50] == "R") {
+            ratos += quantia;
+        } else if (tipo[50] == "S") {
+            sapos += quantia;
+        }
     }
 
     return 0;
