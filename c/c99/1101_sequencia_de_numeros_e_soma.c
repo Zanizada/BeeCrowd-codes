0cbd5f8ea7b526ab8a03efec479837a89bb07202
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 20:22:42 -0300

    File modified in local repository

--- c/c99/1101_sequencia_de_numeros_e_soma.c
+++ c/c99/1101_sequencia_de_numeros_e_soma.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int M, N, ordem, soma;
+    while (M > 0 && N > 0) {
+        scanf("%d %d", &M, &N);
+
+        if (M <= 0 || N <= 0) {
+            break;
+        }
+
+        if (M > N) {
+            int temp = M;
+            M = N;
+            N = temp;
+        }
+
+        for (int i = M; i < N + 1; i++) {
+            ordem[N] = i;
+            soma += i
+        }
+    }
+    return 0;
+}
