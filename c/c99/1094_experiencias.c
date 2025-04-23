5be66f7c16fe95d350c8108d6d10c9cd0408b774
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 16:42:40 -0300

    File modified in local repository

--- c/c99/1094_experiencias.c
+++ c/c99/1094_experiencias.c
@@ -2,10 +2,11 @@
 
 int main() {
     int N, total = 0, coelhos = 0, ratos = 0, sapos = 0, percentual_c = 0, percentual_r = 0, percentual_s = 0;
-    char quantia[50], tipo[50];
+    char tipo[50];
     scanf("%d", &N);
 
     for (int i = 0; i < N; i++) {
+        scanf("%d %s", quantia, tipo[50]);
 
     }
 
