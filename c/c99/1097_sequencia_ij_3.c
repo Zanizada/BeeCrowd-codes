7311359c9ea65a4b20ddcf0413d29dfbab6c7934
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 17:46:03 -0300

    File modified in local repository

--- c/c99/1097_sequencia_ij_3.c
+++ c/c99/1097_sequencia_ij_3.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    int I = 1, J = 7, contador = 0;
 
+    while (I <= 9) {
+        printf("I=%d J=%d\n", I, J);
+        J -= 1;
+        contador += 1;
+
+        if (contador == 3) {
+            I += 2;
+            J += 5;
+            contador = 0;
+        }
+    }
     return 0;
 }
