5b43d2af230d388c5347bf9566407f3cf951fb61
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 16:56:04 -0300

    File modified in local repository

--- c/c99/1094_experiencias.c
+++ c/c99/1094_experiencias.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int N, quantia, total = 0, coelhos = 0, ratos = 0, sapos = 0, percentual_c = 0, percentual_r = 0, percentual_s = 0;
+    int N, quantia, total = 0, coelhos = 0, ratos = 0, sapos = 0;
+    float percentual_c = 0, percentual_r = 0, percentual_s = 0;
     char tipo[50];
     scanf("%d", &N);
 
@@ -16,6 +17,12 @@ int main() {
             sapos += quantia;
         }
     }
+
+    percentual_c = (coelhos*100)/total;
+    percentual_r = (ratos*100)/total;
+    percentual_s = (sapos*100)/total;
+
+    printf("Total: %d cobaias\nTotal de coelhos: %d\nTotal de ratos: %d\nTotal de sapos: %d\nPercentual de coelhos: %.2f %%\nPercentual de ratos: %.2f %%\nPercentual de sapos: %.2f %%\n", total, coelhos, ratos, sapos, percentual_c, percentual_r, percentual_s);
 
     return 0;
 }
