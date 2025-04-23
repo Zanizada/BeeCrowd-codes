3a97c5f0620e7c6b26b30bc1ef3f8498a5b0b63b
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 08:36:06 -0300

    New item listed in local repository

--- c/c99/1065_pares_entre_cinco_numeros.c
+++ c/c99/1065_pares_entre_cinco_numeros.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    int N, pares = 0;
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &N);
+        if (N % 2 == 0) pares++;
+    }
+
+    printf("%d valores pares\n", pares);
+    return 0;
+}
