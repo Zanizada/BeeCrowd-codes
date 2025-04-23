ebd7fac388d2796dfce71d51c99d931c74bdadae
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 11:10:02 -0300

    File modified in local repository

--- c/c99/1073_quadrado_de_pares.c
+++ c/c99/1073_quadrado_de_pares.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+    int N, multi;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        if (i % 2 == 0) {
+            multi = i * i;
+            printf("%d^2 = %d\n", i, multi);
+        }
+    }
+}
