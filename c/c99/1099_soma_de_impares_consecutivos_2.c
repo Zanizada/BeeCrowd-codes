4b1036fb7bbd6887fb16dd664968eeaae95eae0a
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 19:16:04 -0300

    File modified in local repository

--- c/c99/1099_soma_de_impares_consecutivos_2.c
+++ c/c99/1099_soma_de_impares_consecutivos_2.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int temp, soma, N, X, Y, i;
+    int N, X, Y;
     scanf("%d", &N);
-    for (i == 0; i < N; i++) {
+
+    for (int i = 0; i < N; i++) {
         scanf("%d %d", &X, &Y);
-        for (i = X + 1; i < Y; i++) {
-            if (i % 2 != 0) {
-                soma += i
+
+        int soma = 0;
+
+        if (X > Y) {
+            int temp = X;
+            X = Y;
+            Y = temp;
+        }
+
+        for (int j = X + 1; j < Y; j++) {
+            if (j % 2 != 0) {
+                soma += j;
             }
         }
+
         printf("%d\n", soma);
     }
+
     return 0;
 }
