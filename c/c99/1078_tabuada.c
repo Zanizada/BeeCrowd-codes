ac35e3afd0f6fdcd3e9a9a9f49a2c1c86dd6f804
Zanizada <rafaelgizzi@gmail.com>
2025-04-23 14:20:03 -0300

    New item listed in local repository

--- c/c99/1078_tabuada.c
+++ c/c99/1078_tabuada.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int N, multiplo = 1;
+    int N, multiplo = 1, tabuada;
     scanf("%d", &N);
 
     for (int i = 0; i < 10; i++) {
         tabuada = multiplo * N;
-        printf("%d x %d = %d", multiplo, N, tabuada);
+        printf("%d x %d = %d\n", multiplo, N, tabuada);
         multiplo++;
     }
 
