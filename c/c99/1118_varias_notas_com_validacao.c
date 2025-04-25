3b748dee27ace5120d04b0f1c88bd916a66953a9
Zanizada <rafaelgizzi@gmail.com>
2025-04-25 10:16:08 -0300

    File modified in local repository

--- c/c99/1118_varias_notas_com_validacao.c
+++ c/c99/1118_varias_notas_com_validacao.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    double N, soma = 0, media;
+    int divisor = 0;
+
+    while (1) {
+        scanf("%lf", &N);
+
+        if (0 > N || N > 10) {
+            printf("nota invalida\n");
+        } else {
+            divisor++;
+            soma += N;
+
+            if (divisor == 2) {
+                media = soma / divisor;
+                printf("media = %.2lf\n", media);
+            }
+            int X;
+            scanf("novo calculo (1-sim 2-nao)%d", &X);
+            if (X < 1 || X > 2) {
+                scanf("novo calculo (1-sim 2-nao)%d", &X);
+            } else {
+                if (X)
+            }
+        }
+    }
+    return 0;
+}
