bdaf965e4fe7b010e8ab23d73ac07f84de92ed45
Zanizada <rafaelgizzi@gmail.com>
2025-04-25 11:12:42 -0300

    File modified in local repository

--- c/c99/1131_grenais.c
+++ c/c99/1131_grenais.c
@@ -9,9 +9,10 @@ int main() {
             emp++;
         } else if (INT > GRE) {
             vit++;
-        } else {
+        } else if (INT < GRE) {
             der++;
         }
+
         do {
             printf("novo grenal (1-sim 2-nao)\n");
             scanf("%d", &X);
@@ -30,7 +31,7 @@ int main() {
         printf("Nao houve vencedor\n");
     } else if (vit > der) {
         printf("Inter venceu mais\n");
-    } else {
+    } else if (vit < der) {
         printf("Gremio venceu mais\n");
     }
     return 0;
