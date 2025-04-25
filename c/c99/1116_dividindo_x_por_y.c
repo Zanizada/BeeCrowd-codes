b2599c48ed4c14f42b36865e2788ebfef2fb3420
Zanizada <rafaelgizzi@gmail.com>
2025-04-25 09:40:02 -0300

    File modified in local repository

--- c/c99/1116_dividindo_x_por_y.c
+++ c/c99/1116_dividindo_x_por_y.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int N, X, Y, div;
+    int N, X, Y;
+    float div;
+    scanf("%d", &N);
 
     for (int i = 0; i < N; i++) {
+        scanf("%d %d", &X, &Y);
 
+        if (Y == 0) {
+            printf("divisao impossivel\n");
+        } else {
+            div = (float)X / Y;
+            printf("%0.1f\n", div);
+        }
     }
+
     return 0;
 }
