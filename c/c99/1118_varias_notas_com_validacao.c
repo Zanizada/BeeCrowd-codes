57f9fce9272ac959a2e297e23b0fbe7482632412
Zanizada <rafaelgizzi@gmail.com>
2025-04-25 10:20:02 -0300

    File modified in local repository

--- c/c99/1118_varias_notas_com_validacao.c
+++ c/c99/1118_varias_notas_com_validacao.c
@@ -22,7 +22,23 @@ int main() {
             if (X < 1 || X > 2) {
                 scanf("novo calculo (1-sim 2-nao)%d", &X);
             } else {
-                if (X)
+                if (X == 2) {
+                    break;
+                } else {
+                    scanf("%lf", &N);
+                    divisor = 0;
+                    soma = 0;
+
+                    if (0 > N || N > 10) {
+                        printf("nota invalida\n");
+                    } else {
+                        divisor++;
+                        soma += N;
+
+                        if (divisor == 2) {
+                            media = soma / divisor;
+                            printf("media = %.2lf\n", media);
+                }
             }
         }
     }
