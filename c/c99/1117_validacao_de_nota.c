0f32c30b669d0faa96224b8573481c3ca27fc9ac
Zanizada <rafaelgizzi@gmail.com>
2025-04-25 10:00:02 -0300

    File modified in local repository

--- c/c99/1117_validacao_de_nota.c
+++ c/c99/1117_validacao_de_nota.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    double N, soma = 0, media;
+    int divisor = 0;
+    while (1) {
+        scanf("%lf", &N);
+
+        if (0 > N || N > 10) {
+            printf("nota invalida\n");
+        } else {
+            divisor++;
+            soma += N;
+            if (divisor == 2) {
+                media = soma/(float)divisor;
+                printf("media = %.2lf\n");
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
