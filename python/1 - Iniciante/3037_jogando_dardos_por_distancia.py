994761a306b3bbade2c4dabc7fe5ee215bb646ba
Zanizada <rafaelgizzi@gmail.com>
2025-04-29 15:32:42 -0300

    File modified in local repository

--- python/1 - Iniciante/3037_jogando_dardos_por_distancia.py
+++ python/1 - Iniciante/3037_jogando_dardos_por_distancia.py
@@ -1,1 +1,19 @@
 N = int(input())
+arremessosJoao = 0
+arremessosMaria = 0
+
+for i in range(N):
+    for arremessos in range(6):
+
+        for arremessosJ in range(3):
+            X, D = map(int, input().split())
+            arremessosJoao += X
+        for arremessosM in range(3):
+            X, D = map(int, input().split())
+            arremessosMaria += X
+
+        if arremessosJoao > arremessosMaria:
+            vitoria = 'JOAO'
+        elif arremessosJoao < arremessosMaria:
+               qvitoria = 'MARIA'
+        print(vitoria)
