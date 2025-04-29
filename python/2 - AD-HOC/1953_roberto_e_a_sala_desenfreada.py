b93b30d9ffee87642421fdc0ef5dea72b0641c74
Zanizada <rafaelgizzi@gmail.com>
2025-04-29 12:36:04 -0300

    File modified in local repository

--- python/2 - AD-HOC/1953_roberto_e_a_sala_desenfreada.py
+++ python/2 - AD-HOC/1953_roberto_e_a_sala_desenfreada.py
@@ -1,19 +1,27 @@
-qntdDeAlunos = int(input())
-EPR = 0
-EHD = 0
-INTRUSOS = 0
+import sys
 
-for i in range(qntdDeAlunos):
-    matricula, curso = input().split();
-    matricula = int(matricula);
+while True:
+    try:
+        qntdDeAlunos = int(input())
 
-    if curso == 'EPR':
-            EPR += 1
-    elif curso == 'EHD':
-            EHD += 1
-    else:
-        INTRUSOS += 1
+        EPR = 0
+        EHD = 0
+        INTRUSOS = 0
 
-print(f'EPR: {EPR}')
-print(f'EHD: {EHD}')
-print(f'INTRUSOS: {INTRUSOS}')
+        for i in range(qntdDeAlunos):
+            matricula, curso = sys.stdin.readline().split()
+            matricula = int(matricula)
+
+            if curso == 'EPR':
+                EPR += 1
+            elif curso == 'EHD':
+                EHD += 1
+            else:
+                INTRUSOS += 1
+
+        print(f'EPR: {EPR}')
+        print(f'EHD: {EHD}')
+        print(f'INTRUSOS: {INTRUSOS}')
+
+    except EOFError:
+        break
