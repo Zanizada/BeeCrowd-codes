266aad7d781a804e1fb30779b4f2568735c2f133
Zanizada <rafaelgizzi@gmail.com>
2025-04-30 16:30:04 -0300

    File modified in local repository

--- python/3 - Strings/1120_revisao_de_contrato.py
+++ python/3 - Strings/1120_revisao_de_contrato.py
@@ -1,7 +1,6 @@
 while True:
     D, N = map(int, input().split())
     if D == 0 and N == 0:
-        print(0)
         break
 
     D = str(D)
@@ -9,7 +8,10 @@ while True:
     
     valorListado = [int(num) for num in N if num != D]
 
-    if all(num == 0 for num in valorListado):
+    if N == D:
         print(0)
+
+    if valorListado and not all(num == 0 for num in valorListado):
+        print(int("".join(map(str, valorListado))))
     else:
-        print("".join(map(str, valorListado)))
+        print(0)
