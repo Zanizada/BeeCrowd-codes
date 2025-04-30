d0b1d18460106e7580e75bef333576b2d4d517a7
Zanizada <rafaelgizzi@gmail.com>
2025-04-30 10:05:32 -0300

    New item listed in local repository

--- python/2 - AD-HOC/1285_digitos_diferentes.py
+++ python/2 - AD-HOC/1285_digitos_diferentes.py
@@ -1,12 +1,18 @@
+import sys
+
 def digitosUnicos(numero):
     num_str = str(numero)
 
     return len(num_str) == len(set(num_str))
 
 while True:
-    N, M = map(int, input().split())
-    numerosValidos = 0
-    for i in range(N, M+1):
-        if digitosUnicos(i) == True:
-            numerosValidos += 1
-    print(numerosValidos)
+    try:
+        N, M = map(int, input().split())
+        numerosValidos = 0
+        for i in range(N, M+1):
+            if digitosUnicos(i) == True:
+                numerosValidos += 1
+        print(numerosValidos)
+
+    except EOFError:
+        break
