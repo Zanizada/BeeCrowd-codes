e7b2eab5bd4b62fc023fe37af623a060dfa4d828
Zanizada <rafaelgizzi@gmail.com>
2025-05-01 01:40:03 -0300

    File modified in local repository

--- python/3 - Strings/1141_cultivando_strings.py
+++ python/3 - Strings/1141_cultivando_strings.py
@@ -1,29 +1,24 @@
-def is_prefix(a, b):
-    return b.startswith(a)
-
-def dfs(i, strings, memo):
-    if memo[i] != -1:
-        return memo[i]
-
-    max_len = 1
-    for j in range(len(strings)):
-        if i != j and is_prefix(strings[i], strings[j]):
-            max_len = max(max_len, 1 + dfs(j, strings, memo))
-
-    memo[i] = max_len
-    return max_len
-
 while True:
     N = int(input())
     if N == 0:
         break
 
     strings = [input().strip() for _ in range(N)]
-    strings.sort()
+    usadas = [False] * N
+    maior = 0
 
-    memo = [-1] * N
-    resposta = 0
     for i in range(N):
-        resposta = max(resposta, dfs(i, strings, memo))
+        if not usadas[i]:
+            base = strings[i]
+            grupo = [i]
 
-    print(resposta)
+            for j in range(N):
+                if i != j and not usadas[j] and base in strings[j]:
+                    grupo.append(j)
+
+            for idx in grupo:
+                usadas[idx] = True
+
+            maior = max(maior, len(grupo))
+
+    print(maior)
