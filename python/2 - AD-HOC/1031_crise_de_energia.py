a825bcef41ba690dfc624d05b82596a02b84aacf
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-13 16:39:39 -0300

    File modified in local repository

--- python/2 - AD-HOC/1031_crise_de_energia.py
+++ python/2 - AD-HOC/1031_crise_de_energia.py
@@ -1,15 +1,23 @@
 import random
 
+def numeros_sequenciais(condicao):
+    num = 1
+    while condicao:
+        num += 1
+    return num
+
 def josephus(tamanho_lista, salto, lista):
     if lista != None:
         lista = list(range(1, tamanho_lista + 1))
     else:
         numeros_josephus = 0
-
         while len(lista) > 1:
             numeros_josephus = (numeros_josephus + salto - 1) % len(lista)
             del lista[numeros_josephus]
         
     return {lista[0]}
 
-qntd_regioes = 
+while True:
+    qntd_regioes = int(input())
+    regioes = []
+    
