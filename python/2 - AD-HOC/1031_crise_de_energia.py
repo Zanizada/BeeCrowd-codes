ea7f413a7336ba8ac3d0d7680b5d492db0f80552
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-13 16:06:29 -0300

    File modified in local repository

--- python/2 - AD-HOC/1031_crise_de_energia.py
+++ python/2 - AD-HOC/1031_crise_de_energia.py
@@ -1,35 +1,25 @@
 import random
 
-def num_de_wellington(lista):
-    if 13 in lista:
-        if lista[-1] == 13:
-            pass
-        else:
-            lista.remove(13)
-            lista.append(13)
-
 def josephus(lista, salto):
-    num_de_wellington(lista)
     indice = 0
     while len(lista) > 1:
         indice = (indice + salto - 1) % len(lista)
-        if lista[indice] == 13:
-            lista.append(lista.pop(indice))
-        else:
-            lista.pop(indice)
+        lista.pop(indice)
     return lista[0]
 
 while True:
-    regioes = []
     num_regioes = int(input())
-    
+
     if num_regioes == 0:
         break
-    
-    num_salto = random.randint(1, num_regioes)
 
-    for num in range(num_regioes):
-        regioes.append(num)
-    
-    resultado = josephus(regioes, num_salto)
-    print(resultado)
+    menor_m = None
+    for _ in range(1000):  # Limite de tentativas para encontrar o menor m
+        m = random.randint(1, num_regioes)  # Gera um valor aleatório para m
+        regioes = list(range(1, num_regioes + 1))
+        resultado = josephus(regioes, m)
+        if resultado == 13:
+            if menor_m is None or m < menor_m:
+                menor_m = m
+
+    print(menor_m)
