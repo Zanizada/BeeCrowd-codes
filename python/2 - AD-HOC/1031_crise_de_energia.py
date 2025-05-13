2b8dcff50242c947f7e751027c3c56fd7b3e5db3
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-13 16:19:36 -0300

    File modified in local repository

--- python/2 - AD-HOC/1031_crise_de_energia.py
+++ python/2 - AD-HOC/1031_crise_de_energia.py
@@ -1,4 +1,11 @@
-def josephus(lista, salto):
+def josephus(tamanho_lista, salto):
+    lista = list(range(1, tamanho_lista + 1))
+    numeros_josephus = 0
+
     while len(lista) > 1:
-        ultimo_valor = (ultimo_valor + salto - 1) % len(lista)
-        del lista[ultimo_valor]
+        numeros_josephus = (numeros_josephus + salto - 1) % len(lista)
+        del lista[numeros_josephus]
+    
+    return {lista[0]}
+
+print(josephus(int(input()), int(input())))
