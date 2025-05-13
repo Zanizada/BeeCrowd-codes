54a3bd5c14eca42a1103b1568e34e1f46eb8e730
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-13 09:49:36 -0300

    File modified in local repository

--- python/2 - AD-HOC/1026_carrega_ou_nao_carrega.py
+++ python/2 - AD-HOC/1026_carrega_ou_nao_carrega.py
@@ -2,9 +2,26 @@ a = 6
 b = 4
 
 def XOR(a, b):
-    bin_a = bin(a)
-    bin_b = bin(b)
+    bits_c = 0
+    total_bits = []
+    bin_a = bin(a)[2:].zfill(32)
+    bin_b = bin(b)[2:].zfill(32)
 
     for bit_a, bit_b in zip(bin_a, bin_b):
         bit_a, bit_b = int(bit_a), int(bit_b)
-        
+        if bit_a == bit_b:
+            total_bits.append(0)
+        else:
+            total_bits.append(1)
+    bits_c.join(total_bits)
+    bin_c = str(bits_c)
+    int_c = int(bin_c, 2)
+    return int_c
+
+try:
+    A = int(input())
+    B = int(input())
+    resultado = XOR(A, B)
+    print(resultado)
+
+except EOFError:
