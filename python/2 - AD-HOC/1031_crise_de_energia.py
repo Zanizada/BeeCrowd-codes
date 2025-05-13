d70235e9fb423d3a702d5053aa26bcf2bdb4eba6
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-13 16:10:03 -0300

    File modified in local repository

--- python/2 - AD-HOC/1031_crise_de_energia.py
+++ python/2 - AD-HOC/1031_crise_de_energia.py
@@ -1,25 +1,4 @@
-import random
-
 def josephus(lista, salto):
-    indice = 0
     while len(lista) > 1:
-        indice = (indice + salto - 1) % len(lista)
-        lista.pop(indice)
-    return lista[0]
-
-while True:
-    num_regioes = int(input())
-
-    if num_regioes == 0:
-        break
-
-    menor_m = None
-    for _ in range(1000):  # Limite de tentativas para encontrar o menor m
-        m = random.randint(1, num_regioes)  # Gera um valor aleatório para m
-        regioes = list(range(1, num_regioes + 1))
-        resultado = josephus(regioes, m)
-        if resultado == 13:
-            if menor_m is None or m < menor_m:
-                menor_m = m
-
-    print(menor_m)
+        ultimo_valor = (ultimo_valor + salto - 1) % len(lista)
+        del lista[ultimo_valor]
