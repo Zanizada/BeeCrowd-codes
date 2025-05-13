de24bcef5e89bd7b138f7cd6cdaf280ccfa89ee7
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-13 16:50:03 -0300

    File modified in local repository

--- python/2 - AD-HOC/1031_crise_de_energia.py
+++ python/2 - AD-HOC/1031_crise_de_energia.py
@@ -6,18 +6,22 @@ def numeros_sequenciais(condicao):
         num += 1
     return num
 
-def josephus(tamanho_lista, salto, lista):
-    if lista != None:
-        lista = list(range(1, tamanho_lista + 1))
+def josephus(salto, tamanho_lista=None, lista=None):
+    if lista is None and tamanho_lista is None:
+        raise ValueError("Você precisa fornecer 'lista' ou 'tamanho_lista'.")
     else:
-        numeros_josephus = 0
-        while len(lista) > 1:
-            numeros_josephus = (numeros_josephus + salto - 1) % len(lista)
-            del lista[numeros_josephus]
+        if lista is None:
+            lista = list(range(1, tamanho_lista + 1))
+        elif tamanho_lista is None:
+            tamanho_lista = len(lista)
         
+    numeros_josephus = 0
+    while len(lista) > 1:
+        numeros_josephus = (numeros_josephus + salto - 1) % len(lista)
+        del lista[numeros_josephus]
     return {lista[0]}
 
 while True:
     qntd_regioes = int(input())
     regioes = []
-    
+    salto = numeros_sequenciais(())
