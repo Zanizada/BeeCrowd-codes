54fa4a48eb73f943fe50343367d94381aeb5fdc9
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-13 14:59:36 -0300

    File modified in local repository

--- python/2 - AD-HOC/1030_a_lenda_de_flavious_josephus.py
+++ python/2 - AD-HOC/1030_a_lenda_de_flavious_josephus.py
@@ -1,18 +1,17 @@
 casos_teste = int(input())
 sobreviventes = []
-sobrevivente = None
+sobrevivente = 0
 
 for i in range(casos_teste):
     qntd_pessoas, salto = map(int, input().split())
     
     for pessoas in range(qntd_pessoas):
         sobreviventes.append(pessoas)
-        sobrevivente =+ pessoas
+        sobrevivente += pessoas
 
-    tamanho_lista = len(sobreviventes)
-
-    while tamanho_lista > 1:
-        sobrevivente -= sobreviventes[salto]
-
+    
+    while len(sobreviventes) > 1:
+        sobrevivente = (sobrevivente + salto - 1) % len(sobreviventes)
+        del sobreviventes(sobrevivente)
     #prints dos casos
-    print(f'Case {i}: {sobrevivente}')
+    print(f'Case {i}: {sobreviventes[0]}')
