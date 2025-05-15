9e84f77f8be48458e55cf4be56f5d31950d73c72
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-15 11:29:21 -0300

    solved 1032 beecrowd problem

--- python/2 - AD-HOC/1032_o_primo_de_josephus.py
+++ python/2 - AD-HOC/1032_o_primo_de_josephus.py
@@ -1,28 +1,34 @@
-def josephus(qntd_pessoas, salto):
-    sobreviventes = list(range(1, qntd_pessoas + 1))
-    indice = 0
-
-    while len(sobreviventes) > 1:
-        indice = (indice + salto - 1) % len(sobreviventes)
-        del sobreviventes[indice]
-
-    return sobreviventes[0]
-
 def numero_primo(numero):
-    if numero > 1:
-        for divisor in range(2, numero):
-            if numero % divisor != 0:
-                return True
-            else:
-                return False
-    else:
+    if numero < 2:
         return False
-    
-numeros = list(input().split())
-for num in numeros:
-    num = int(num)
-    primo = numero_primo(num)
-    if primo == True:
-        print(num)
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+def proximo_primo(atual):
+    candidato = atual + 1
+    while True:
+        if numero_primo(candidato):
+            return candidato
+        candidato += 1
+
+def primo_josephus(qntd_pessoas):
+    pessoas = list(range(1, qntd_pessoas + 1))
+    indice = 0
+    salto = 2  # primeiro primo
+    while len(pessoas) > 1:
+        indice = (indice + salto - 1) % len(pessoas)
+        pessoas.pop(indice)
+        salto = proximo_primo(salto)
+    return pessoas[0]
+
+while True:
+    qntd_pessoas = int(input())
+
+    if qntd_pessoas == 0:
+        break
     else:
         pass
+    
+    print(primo_josephus(qntd_pessoas))
