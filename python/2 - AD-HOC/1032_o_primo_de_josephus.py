a2cc13ff78991806dcbf95cdf2b856cb62b566c6
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-15 10:39:47 -0300

    File modified in local repository

--- python/2 - AD-HOC/1032_o_primo_de_josephus.py
+++ python/2 - AD-HOC/1032_o_primo_de_josephus.py
@@ -1,2 +1,12 @@
 def josephus(qntd_pessoas, salto):
     sobreviventes = list(range(1, qntd_pessoas + 1))
+    indice = 0
+
+    while len(sobreviventes) > 1:
+        indice = (indice + salto - 1) % len(sobreviventes)
+        del sobreviventes[indice]
+
+    return sobreviventes[0]
+
+def numero_primo(numero):
+    
