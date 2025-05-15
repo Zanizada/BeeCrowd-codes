757adadb667ffce3e6aa58f23ac2dafa7e292c88
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-15 11:00:03 -0300

    File modified in local repository

--- python/2 - AD-HOC/1032_o_primo_de_josephus.py
+++ python/2 - AD-HOC/1032_o_primo_de_josephus.py
@@ -9,4 +9,16 @@ def josephus(qntd_pessoas, salto):
     return sobreviventes[0]
 
 def numero_primo(numero):
+    if numero > 1:
+        for divisor in range(2, numero):
+            if numero % divisor != 0:
+                return True
+            else:
+                return False
+    else:
+        return False
     
+numeros = int(input().split())
+for num in numeros:
+    primo = numero_primo(num)
+print(num)
