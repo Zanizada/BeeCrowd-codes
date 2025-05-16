f663938bc5fb117beb1c2254d2b6223be1da809a
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-16 09:50:01 -0300

    File modified in local repository

--- python/2 - AD-HOC/1086_o_salao_do_clube.py
+++ python/2 - AD-HOC/1086_o_salao_do_clube.py
@@ -1,16 +1,24 @@
 def conversao_m_para_cm(numero=None, numeros=None):
-    if numero is None:
+    if numero is None and numeros is not None:
         metros = []
         for num in numeros:
             centimetros = num * 100
             metros.append(centimetros)
         return metros
-    elif numeros is None:
+    elif numeros is None and numero is not None:
         metro = numero * 100
         return metro
+    else:
+        raise ValueError()
 
 while True:
     N, M = map(int, input().split())
+
+    if N == 0 and M == 0:
+        break
+    else:
+        pass
+
     L = int(input())
     K = int(input())
     for i in range(K):
