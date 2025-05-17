fb6eae21d197533f2e422d6991a4b8ec1722e3e3
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-16 22:50:01 -0300

    File modified in local repository

--- python/2 - AD-HOC/1086_o_salao_do_clube.py
+++ python/2 - AD-HOC/1086_o_salao_do_clube.py
@@ -1,70 +1,83 @@
-from collections import Counter
-
 def conversao_m_para_cm(numero):
     if isinstance(numero, list):
         return [n * 100 for n in numero]
     else:
         return numero * 100
 
-def resolver(c, r, largura_tabuas_cm, tabuas):
-    if c % largura_tabuas_cm != 0:
-        return 'impossivel'
-
-    fileiras = c // largura_tabuas_cm
-    comprimento_necessario = r
-
-    tabuas_validas = [t for t in tabuas if t <= comprimento_necessario]
-
-    contagem = Counter(tabuas_validas)
-    usadas = 0
-
-    usar_simples = min(contagem[comprimento_necessario], fileiras)
-    usadas += usar_simples
-    contagem[comprimento_necessario] -= usar_simples
-    fileiras -= usar_simples
-
-    if fileiras == 0:
-        return usadas
-
-    comprimentos = sorted(contagem.keys())
-    i, j = 0, len(comprimentos) - 1
-
-    while i <= j and fileiras > 0:
-        a, b = comprimentos[i], comprimentos[j]
-        if a + b < comprimento_necessario:
-            i += 1
-        elif a + b > comprimento_necessario:
-            j -= 1
-        else:
-            if a == b:
-                pares = contagem[a] // 2
-            else:
-                pares = min(contagem[a], contagem[b])
-
-            usados_pares = min(pares, fileiras)
-            usadas += usados_pares * 2
-            contagem[a] -= usados_pares
-            contagem[b] -= usados_pares
-            fileiras -= usados_pares
-
-            if contagem[a] == 0: i += 1
-            if contagem[b] == 0: j -= 1
-
-    return usadas if fileiras == 0 else 'impossivel'
-
+def encontrar_menor_numero_possivel(lista, alvo):
+    lista.sort()
+    from collections import Counter
+    contador = Counter(lista)
+    usados = 0
+    while alvo > 0 and lista:
+        valor = lista.pop()
+        if valor == alvo:
+            usados += 1
+            return usados
+        elif valor < alvo:
+            complemento = alvo - valor
+            if complemento in contador and (complemento != valor or contador[complemento] > 1):
+                usados += 2
+                return usados
+    return float('inf')
 
 while True:
-    largura, comprimento = map(int, input().split())
-    if largura == 0 and comprimento == 0:
+    try:
+        linha = input()
+        if not linha.strip():
+            continue
+        largura, comprimento = map(int, linha.strip().split())
+        if largura == 0 and comprimento == 0:
+            break
+
+        largura_tabuas_cm = int(input())
+        tabuas_doadas = int(input())
+        comprimento_tabuas = list(map(int, input().split()))
+        
+        largura_cm = conversao_m_para_cm(largura)
+        comprimento_cm = conversao_m_para_cm(comprimento)
+        comprimento_tabuas_cm = conversao_m_para_cm(comprimento_tabuas)
+
+        if largura_cm % largura_tabuas_cm != 0:
+            print("impossivel")
+            continue
+
+        num_fileiras = largura_cm // largura_tabuas_cm
+        comprimento_tabuas_cm.sort()
+        from collections import Counter
+        contador = Counter(comprimento_tabuas_cm)
+
+        usadas = 0
+        sucesso = True
+
+        for _ in range(num_fileiras):
+            encontrado = False
+            for t in reversed(comprimento_tabuas_cm):
+                if contador[t] == 0:
+                    continue
+                if t == comprimento_cm:
+                    contador[t] -= 1
+                    usadas += 1
+                    encontrado = True
+                    break
+                elif t < comprimento_cm:
+                    resto = comprimento_cm - t
+                    if resto == t and contador[t] >= 2:
+                        contador[t] -= 2
+                        usadas += 2
+                        encontrado = True
+                        break
+                    elif resto != t and contador[resto] > 0:
+                        contador[t] -= 1
+                        contador[resto] -= 1
+                        usadas += 2
+                        encontrado = True
+                        break
+            if not encontrado:
+                sucesso = False
+                break
+
+        print(usadas if sucesso else "impossivel")
+
+    except EOFError:
         break
-
-    largura_tabuas_cm = int(input())
-    tabuas_doadas = int(input())
-    comprimento_tabuas = list(map(int, input().split()))
-
-    largura_cm = conversao_m_para_cm(largura)
-    comprimento_cm = conversao_m_para_cm(comprimento)
-    comprimento_tabuas_cm = conversao_m_para_cm(comprimento_tabuas)
-
-    resultado = resolver(largura_cm, comprimento_cm, largura_tabuas_cm, comprimento_tabuas_cm)
-    print(resultado)
