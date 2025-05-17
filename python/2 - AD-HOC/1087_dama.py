7fd5454dcd0507e05d1fec997e2f61863ca0dbd5
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-17 01:10:01 -0300

    File modified in local repository

--- python/2 - AD-HOC/1087_dama.py
+++ python/2 - AD-HOC/1087_dama.py
@@ -1,31 +1,19 @@
-from colorama import init, Back, Style
+def tabuleiro_xadrez():
+    tabuleiro = []
+    for linha in range(1, 9):
+        for coluna in range(1, 9):
+            tabuleiro.append((linha, coluna))
+    for posicao in tabuleiro:
+        print(posicao)
 
-init()
-
-for linha in range(8):
-    for coluna in range(8):
-        if (linha + coluna) % 2 == 0:
-            print(Back.WHITE + '   ', end='')
-        else:
-            print(Back.BLACK + '   ', end='')
-    print(Style.RESET_ALL)
-
-# def tabuleiro_xadrez():
-#     tabuleiro = []
-#     for linha in range(1, 9):
-#         for coluna in range(1, 9):
-#             tabuleiro.append((linha, coluna))
-#     for posicao in tabuleiro:
-#         print(posicao)
-
-# while True:
-#     try:
-#         X1, Y1, X2, Y2 = map(int, input().split())
+while True:
+    try:
+        X1, Y1, X2, Y2 = map(int, input().split())
         
-#         if X1 == Y1 == X2 == Y2 == 0:
-#             break
+        if X1 == Y1 == X2 == Y2 == 0:
+            break
         
-#         tabuleiro_xadrez()
+        tabuleiro_xadrez()
 
-#     except EOFError:
-#         break
+    except EOFError:
+        break
