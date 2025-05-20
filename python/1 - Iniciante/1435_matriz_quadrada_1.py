132a6f507a262d7109742c9554a4611c2c1dc295
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-20 10:40:01 -0300

    File modified in local repository

--- python/1 - Iniciante/1435_matriz_quadrada_1.py
+++ python/1 - Iniciante/1435_matriz_quadrada_1.py
@@ -1,3 +1,13 @@
+def matriz(ordem):
+    linha = '1'
+    for i in range(ordem):
+        linhas = i * linha
+        print(linhas, sep='   ')
+
 while True:
     ordens_matrizes = int(input())
     
+    if ordens_matrizes == 0:
+        break
+
+    matriz(ordens_matrizes)
