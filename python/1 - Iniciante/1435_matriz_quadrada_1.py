7be0ba06df9de3ed84331a0638ec96242bf333bc
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-20 11:00:01 -0300

    File modified in local repository

--- python/1 - Iniciante/1435_matriz_quadrada_1.py
+++ python/1 - Iniciante/1435_matriz_quadrada_1.py
@@ -1,13 +1,37 @@
 def matriz(ordem):
-    linha = '1'
-    for i in range(ordem):
-        linhas = i * linha
-        print(linhas, sep='   ')
+    matriz = []
+    for linhas_ in range(ordem):
+        linha = []
+        for colunas_ in range(ordem):
+            linha.append(0)
+        matriz.append(linha)
 
+    for linhas in range(ordem):
+        for colunas in range(ordem):
+            camada = min(linhas, colunas, ordem - 1 - linhas, ordem - 1 - colunas)
+            matriz[linhas][colunas] = camada + 1
+
+    for i in range(ordem):
+        for j in range(ordem):
+            if j == 0:
+                # Sem espaço no início
+                print(f"{matriz[i][j]:>3}", end='')
+            else:
+                # Um espaço entre os números
+                print(f" {matriz[i][j]:>3}", end='')
+        print()
+
+# Loop para ler entradas e processar cada matriz
+first = True
 while True:
     ordens_matrizes = int(input())
     
     if ordens_matrizes == 0:
         break
+
+    if not first:
+        print()  # Linha em branco entre as matrizes
+    else:
+        first = False
 
     matriz(ordens_matrizes)
