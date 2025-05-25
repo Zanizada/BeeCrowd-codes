4fe55386f1d438fa71ba34db297799e917589ec9
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-25 14:00:02 -0300

    File modified in local repository

--- python/1 - Iniciante/1541_construindo_casas.py
+++ python/1 - Iniciante/1541_construindo_casas.py
@@ -2,7 +2,10 @@ casos_teste = None
 while True:
     try:
         casos_teste = int(input())
-        A, B, C = map(int, input().split())
+        largura, comprimento, percentual = map(int, input().split())
+
+        area_da_casa = largura*comprimento
+        area_terreno = area_da_casa/(percentual/100)
         
     except casos_teste == 0:
         break
