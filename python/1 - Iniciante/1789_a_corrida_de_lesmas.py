99b670e1944239a2fa6cd5fad814b48aa858faf4
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-25 15:30:02 -0300

    New item listed in local repository

--- python/1 - Iniciante/1789_a_corrida_de_lesmas.py
+++ python/1 - Iniciante/1789_a_corrida_de_lesmas.py
@@ -1,5 +1,16 @@
 while True:
     try:
         qntd_lesmas = int(input())
+        lesmas = list(map(int, input().split()))
+
+        lesma_rapida = max(lesmas)
+
+        if lesma_rapida < 10:
+            print("1")
+        elif 10 <= lesma_rapida < 20:
+            print("2")
+        else:
+            print("3")
+
     except EOFError:
         break
