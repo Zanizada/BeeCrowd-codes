75d0ae0c35ae7a33d860b8066d552458ec7e35d2
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-25 14:10:02 -0300

    File modified in local repository

--- python/1 - Iniciante/1541_construindo_casas.py
+++ python/1 - Iniciante/1541_construindo_casas.py
@@ -1,11 +1,16 @@
-casos_teste = None
+import math
+
 while True:
     try:
-        casos_teste = int(input())
         largura, comprimento, percentual = map(int, input().split())
-
-        area_da_casa = largura*comprimento
-        area_terreno = area_da_casa/(percentual/100)
         
-    except casos_teste == 0:
+        if largura == 0 and comprimento == None and percentual == None:
+            break
+        
+        area_da_casa = math.ceil(largura*comprimento)
+        area_terreno = math.ceil(area_da_casa/(percentual/100))
+        lado_terreno = math.ceil(math.sqrt(area_terreno))
+        print(lado_terreno)
+
+    except EOFError:
         break
