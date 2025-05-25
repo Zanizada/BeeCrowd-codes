b95a77310f79c5a4db8965545ca22b73bdc715e2
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-25 14:50:02 -0300

    New item listed in local repository

--- python/1 - Iniciante/1589_bob_conduite.py
+++ python/1 - Iniciante/1589_bob_conduite.py
@@ -1,10 +1,4 @@
-import math
-
-pi = math.pi
 casos_teste = int(input())
 for _ in range(casos_teste):
     R1, R2 = map(int, input().split())
-    area1 = pi*(R1**2)
-    area2 = pi*(R2**2)
-    area = area1+area2
-    
+    print(R1 + R2)
