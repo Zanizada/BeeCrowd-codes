4e183785c03ea4a09a94219276b7e469569ef2fd
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-25 15:54:36 -0300

    File modified in local repository

---
+++ python/1 - Iniciante/1828_bazinga.py
@@ -0,0 +1,10 @@
+casos_teste = int(input())
+
+for i in casos_teste:
+    sheldon, raj = input().split()
+    "tesoura" > "papel"
+    "papel" > "pedra"
+    "pedra" > "lagarto"
+    "lagarto" > "Spock"
+    "Spock" > "tesoura"
+    "tesoura" > "lagarto"
