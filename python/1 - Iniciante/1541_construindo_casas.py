d4b7a5207afe82585edec4204e64e2b8e00245a3
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-25 13:30:02 -0300

    File modified in local repository

--- python/1 - Iniciante/1541_construindo_casas.py
+++ python/1 - Iniciante/1541_construindo_casas.py
@@ -1,3 +1,7 @@
 casos_teste = None
-while casos_teste != 0:
-    
+while True:
+    try:
+        casos_teste = int(input())
+        A, B, C = map(int, input().split())
+    except casos_teste == 0:
+        break
