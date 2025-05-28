5461c3aa5951bcbe2664dbb49edf62945ce36eea
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-28 01:50:02 -0300

    File modified in local repository

--- python/1 - Iniciante/1866_conta.py
+++ python/1 - Iniciante/1866_conta.py
@@ -2,8 +2,8 @@ casos_teste = int(input())
 
 for _ in range(casos_teste):
     num = int(input())
-    numeros = []
-
-    for _ in range(num):
-        numeros.append(1)
-        
+    
+    if num % 2 == 0:
+        print(0)
+    else:
+        print(1)
