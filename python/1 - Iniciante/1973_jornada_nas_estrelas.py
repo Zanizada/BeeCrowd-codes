4c72f4b20570c857e0f979f235d9a1549c4f8609
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-28 10:40:02 -0300

    File modified in local repository

--- python/1 - Iniciante/1973_jornada_nas_estrelas.py
+++ python/1 - Iniciante/1973_jornada_nas_estrelas.py
@@ -1,8 +1,21 @@
 estrelas = int(input())
 carneiros = list(map(int, input().split()))
 carneiros_roubados = 0
-carneiros_salvos = sum(carneiros)
+estrelas_roubadas = 0
+i = 0
 
-for i in range(estrelas):
-    if carneiros[i] % 2 == 0:
+while i in range(estrelas):
+    if carneiros[i] % 2 != 0:
         carneiros_roubados += 1
+        i += 1
+        estrelas_roubadas += 1
+        carneiros[i] = carneiros[i] - 1
+    else:
+        carneiros_roubados += 1
+        i -= 1
+        estrelas_roubadas += 1
+        carneiros[i] = carneiros[i] - 1
+
+carneiros_salvos = sum(carneiros) - carneiros_roubados
+
+print(carneiros_salvos)
