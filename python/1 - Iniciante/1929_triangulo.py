c65a1b85aa0cff4038c9a6a106fe5b0b5cd7329c
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-28 09:00:03 -0300

    New item listed in local repository

--- python/1 - Iniciante/1929_triangulo.py
+++ python/1 - Iniciante/1929_triangulo.py
@@ -1,3 +1,12 @@
-A, B, C, D = map(int, input().split())
-numeros_ordenados = sorted([A, B, C, D], reverse=True)
-print(numeros_ordenados)
+def verificar_triangulo(a, b, c):
+    return a + b > c and a + c > b and b + c > a
+
+A, B, C, D = sorted(map(int, input().split()), reverse=True)
+
+if (verificar_triangulo(A, B, C) or
+    verificar_triangulo(A, C, D) or
+    verificar_triangulo(B, C, D) or
+    verificar_triangulo(A, B, D)):
+    print("S")
+else:
+    print("N")
