44429ee7e5b64cbdff716d1a072f9e84e2a91d98
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-28 11:30:03 -0300

    File modified in local repository

--- python/1 - Iniciante/1973_jornada_nas_estrelas.py
+++ python/1 - Iniciante/1973_jornada_nas_estrelas.py
@@ -4,7 +4,7 @@ carneiros = list(map(int, input().split()))
 i = 0
 estrelas_visitadas = [0] * estrelas
 
-while 0 <= i < estrelas:
+while True:
     if carneiros[i] > 0:
         carneiros[i] -= 1
         estrelas_visitadas[i] = 1
@@ -13,6 +13,9 @@ while 0 <= i < estrelas:
         i += 1
     else:
         i -= 1
+    
+    if i < -8 or i > 8:
+        break
 
 estrelas_roubadas = sum(estrelas_visitadas)
 carneiros_salvos = sum(carneiros)
@@ -20,17 +23,17 @@ carneiros_salvos = sum(carneiros)
 print(estrelas_roubadas ,carneiros_salvos)
 
 # estrelas = 8
-# carneiros = [1, 3, 5, 7, 11, 13, 17, 19]
+# carneiros = [1, 3, 5, 7, 11, 13, 16, 19]
 # i = 0
 # estrelas_visitadas = [0] * estrelas
-# while 0 <= 0 < estrelas: --> True
+# while 0 <= i < estrelas: --> True
     # if carneiros[0] > 0: --> True
         # carneiros[0] -= 1 --> carneiros[0] = 0
         # estrelas_visitadas[0] = 1
     # if carneiros[0] % 2 != 0: --> False
         # i -= 1
 
-# carneiros = [0, 3, 5, 7, 11, 13, 17, 19]
+# carneiros = [0, 3, 5, 7, 11, 13, 16, 19]
 # estrelas_visitadas = [1, 0, 0, 0, 0, 0, 0, 0]
 
     # if carneiros[-1] > 0: --> 19 > 0 --> True
@@ -39,38 +42,20 @@ print(estrelas_roubadas ,carneiros_salvos)
     # if carneiros[-1] % 2 != 0: --> False
         # i -= 1
 
-# carneiros = [0, 3, 5, 7, 11, 13, 17, 18]
+# carneiros = [0, 3, 5, 7, 11, 13, 16, 18]
 # estrelas_visitadas = [1, 0, 0, 0, 0, 0, 0, 1]
 
-    # if carneiros[-2] > 0: --> 17 > 0 --> True
-        # carneiros[-2] -= 1 --> carneiros[-2] = 16
+    # if carneiros[-2] > 0: --> 16 > 0 --> True
+        # carneiros[-2] -= 1 --> carneiros[-2] = 15
         # estrelas_visitadas[-2] = 1
-    # if carneiros[-2] % 2 != 0: --> False
-        # i -= 1
+    # if carneiros[-2] % 2 != 0: --> True
+        # i += 1
 
-# carneiros = [0, 3, 5, 7, 11, 13, 16, 18]
+# carneiros = [0, 3, 5, 7, 11, 13, 15, 18]
 # estrelas_visitadas = [1, 0, 0, 0, 0, 0, 1, 1]
 
-    # if carneiros[-3] > 0: --> 13 > 0 --> True
-        # carneiros[-3] -= 1 --> carneiros[-3] = 12
-        # estrelas_visitadas[-3] = 1
-    # if carneiros[-3] % 2 != 0: --> False
-        # i -= 1
-
-# carneiros = [0, 3, 5, 7, 11, 12, 16, 18]
-# estrelas_visitadas = [1, 0, 0, 0, 0, 1, 1, 1]
-
-    # if carneiros[-4] > 0: --> 11 > 0 --> True
-        # carneiros[-4] -= 1 --> carneiros[-4] = 10
-        # estrelas_visitadas[-4] = 1
-    # if carneiros[-4] % 2 != 0: --> False
-        # i -= 1
-
-# carneiros = [0, 3, 5, 7, 10, 12, 16, 18]
-# estrelas_visitadas = [1, 0, 0, 0, 1, 1, 1, 1]
-
-    # if carneiros[-5] > 0: --> 7 > 0 --> True
-        # carneiros[-5] -= 1 --> carneiros[-5] = 6
-        # estrelas_visitadas[-5] = 1
-    # if carneiros[-5] % 2 != 0: --> False
-        # i -= 1
+    # if carneiros[-2] > 0: --> 16 > 0 --> True
+        # carneiros[-2] -= 1 --> carneiros[-2] = 15
+        # estrelas_visitadas[-2] = 1
+    # if carneiros[-2] % 2 != 0: --> True
+        # i += 1
