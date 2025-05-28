fd841af186347ec8c412a5d24e39b73322f00476
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-28 01:10:03 -0300

    New item listed in local repository

--- python/1 - Iniciante/1865_mjolnir.py
+++ python/1 - Iniciante/1865_mjolnir.py
@@ -1,3 +1,10 @@
 casos_teste = int(input())
+
 for _ in range(casos_teste):
-    d
+    amigos, forca = input().split()
+    forca = int(forca)
+
+    if amigos == "Thor":
+        print("Y")
+    else:
+        print("N")
