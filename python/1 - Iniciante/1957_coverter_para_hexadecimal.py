c7ba04b162b2686e263c885d799debddebe1702d
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-28 09:30:02 -0300

    New item listed in local repository

--- python/1 - Iniciante/1957_coverter_para_hexadecimal.py
+++ python/1 - Iniciante/1957_coverter_para_hexadecimal.py
@@ -1,3 +1,11 @@
 decimal = int(input())
-hexadecimal = hex(decimal)[-1:]
-print(hexadecimal)
+hexadecimal = hex(decimal)[2:]
+caracteres = []
+for caractere in hexadecimal:
+    if 'a' <= caractere <= 'z':
+        caractere = chr(ord(caractere)-32)
+        caracteres.append(caractere)
+    else:
+        caracteres.append(caractere)
+
+print("".join(caracteres))
