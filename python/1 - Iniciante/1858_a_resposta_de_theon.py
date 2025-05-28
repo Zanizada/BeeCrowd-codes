22c0e1247de3b05787fd09ab3654f4a72b5ae6ed
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-28 00:10:03 -0300

    File modified in local repository

--- python/1 - Iniciante/1858_a_resposta_de_theon.py
+++ python/1 - Iniciante/1858_a_resposta_de_theon.py
@@ -1,1 +1,9 @@
-pessoas = int(input())
+qntd_pessoas = int(input())
+pessoas = list(map(int, input().split()) for _ in range(qntd_pessoas))
+contador = 0
+
+for i in range(1, qntd_pessoas+1):
+    if i in pessoas:
+        contador += 1
+
+print(contador)
