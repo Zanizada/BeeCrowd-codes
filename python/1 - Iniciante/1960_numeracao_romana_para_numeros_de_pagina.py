31342eb1217fce2e6e98ec7640b37bcb8a7c3fe7
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-30 09:40:03 -0300

    File modified in local repository

--- python/1 - Iniciante/1960_numeracao_romana_para_numeros_de_pagina.py
+++ python/1 - Iniciante/1960_numeracao_romana_para_numeros_de_pagina.py
@@ -3,12 +3,10 @@ def int_para_romano(numero):
         1000, 900, 500, 400, 100, 90,
         50, 40, 10, 9, 5, 4, 1
     ]
-
     simbolos = [
         "M", "CM", "D", "CD", "C", "XC",
         "L", "XL", "X", "IX", "V", "IV", "I"
     ]
-
     romano = ""
     for i in range(len(valores)):
         while numero >= valores[i]:
