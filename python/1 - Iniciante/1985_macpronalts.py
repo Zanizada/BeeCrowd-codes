16f74fc0c22aeddb9e032e202002f65665f3b7b1
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-30 09:00:03 -0300

    File modified in local repository

--- python/1 - Iniciante/1985_macpronalts.py
+++ python/1 - Iniciante/1985_macpronalts.py
@@ -1,5 +1,25 @@
-"1001" = 1.50
-"1002" = 2.50
-"1003" = 3.50
-"1004" = 4.50
-"1005" = 5.50
+a = 1.50
+b = 2.50
+c = 3.50
+d = 4.50
+e = 5.50
+
+produtos_comprados = int(input())
+valor_produtos = []
+
+for i in range(produtos_comprados):
+    produto, quantidade = map(int, input().split())
+    if produto == 1001:
+        valor_produtos.append(quantidade*a)
+    elif produto == 1002:
+        valor_produtos.append(quantidade*b)
+    elif produto == 1003:
+        valor_produtos.append(quantidade*c)
+    elif produto == 1004:
+        valor_produtos.append(quantidade*d)
+    elif produto == 1005:
+        valor_produtos.append(quantidade*e)
+
+valor_total = sum(valor_produtos)
+
+print(f"{valor_total:.2f}")
