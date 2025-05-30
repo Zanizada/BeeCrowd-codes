14c13a17b08c9eb6ddfd603c6f4ebf21c8fdfa6d
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-30 09:30:02 -0300

    File modified in local repository

--- python/1 - Iniciante/1985_macpronalts.py
+++ python/1 - Iniciante/1985_macpronalts.py
@@ -1,18 +1,16 @@
 def macPRONALTS(produto, quantidade):
-    produtos = [1001, 1002, 1003, 1004, 1005]
-    valores = [1.50, 2.50, 3.50, 4.50, 5.50]
-    valor_real = 0
-    for i in range(len(produtos)):
-        while produto == produtos[i]:
-            valor_real += valores[i]
-            produto -= produtos[i]
-    return valor_real * quantidade
+    produtos = {
+        1001: 1.50,
+        1002: 2.50,
+        1003: 3.50,
+        1004: 4.50,
+        1005: 5.50
+    }
+    return produtos[produto] * quantidade
 
-produtos_comprados = int(input())
-valores_produtos = []
-for i in range(produtos_comprados):
+produtosComprados = int(input())
+valorTotal = 0
+for _ in range(produtosComprados):
     produto, quantidade = map(int, input().split())
-    valores_produtos.append(macPRONALTS(produto, quantidade))
-valor_total = f"{sum(valores_produtos):.2f}"
-
-print(valor_total)
+    valorTotal += macPRONALTS(produto, quantidade)
+print(f"{valorTotal:.2f}")
