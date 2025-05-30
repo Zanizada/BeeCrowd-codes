d43c99bfc3325997d30fbff08303b7053bd12cc7
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-30 14:00:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2003_domingo_de_manha.py
+++ python/1 - Iniciante/2003_domingo_de_manha.py
@@ -1,6 +1,10 @@
 while True:
     try:
-        horario = int(input()).split(":")
-        
+        horas, minutos = map(int, input().split(":"))
+        if horas >= 7:
+            if 0 < minutos < 60:
+                print(f"Atraso maximo: {minutos}")
+            else:
+                print(f"Atraso maximo: 0")
     except EOFError:
         break
