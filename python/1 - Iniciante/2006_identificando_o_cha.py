a5eeea450967d4026e223150b5630b0b673e5dc3
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-30 18:50:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2006_identificando_o_cha.py
+++ python/1 - Iniciante/2006_identificando_o_cha.py
@@ -1,2 +1,6 @@
-tipo_de_cha = int(input())
-A, B, C, D, E = map(int, input().split())
+cha = int(input())
+a, b, c, d, e = map(int, input().split())
+respostas_corretas = 0
+if a == cha or b == cha or c == cha or d == cha or e == cha:
+    respostas_corretas += 1
+    
