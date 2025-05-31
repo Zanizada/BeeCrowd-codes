fa3aaa60b61097692dbbb5ff7597af147448fd0a
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-30 23:13:36 -0300

    File modified in local repository

--- python/1 - Iniciante/2006_identificando_o_cha.py
+++ python/1 - Iniciante/2006_identificando_o_cha.py
@@ -1,6 +1,8 @@
 cha = int(input())
-a, b, c, d, e = map(int, input().split())
-respostas_corretas = 0
-if a == cha or b == cha or c == cha or d == cha or e == cha:
-    respostas_corretas += 1
-    
+a, b, c, d, e = list(map(int, input().split()))
+chas = [
+    1: "branco",
+    2: "verde",
+    3: "preto",
+    4: "ervas"
+]
