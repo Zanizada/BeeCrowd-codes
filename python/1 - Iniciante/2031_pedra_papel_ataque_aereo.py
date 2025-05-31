5054d0cd1f2a30d60a2de960583e771ac5b599a4
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-31 01:20:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2031_pedra_papel_ataque_aereo.py
+++ python/1 - Iniciante/2031_pedra_papel_ataque_aereo.py
@@ -1,24 +1,23 @@
 casos_teste = int(input())
 
-jokenpo = {
-    "ataque": ["pedra", "papel"],
-    "pedra": ["papel"],
-}
-
-for i in range(casos_teste):
+for _ in range(casos_teste):
     jogador_um = input()
     jogador_dois = input()
 
-    if jogador_um in jokenpo[jogador_dois]:
-        resultado = "Jogador 2 venceu"
-    elif jogador_dois in jokenpo[jogador_um]:
-        resultado = "Jogador 1 venceu"
-    elif jogador_um == jogador_dois:
-        if jogador_um == "papel":
-            resultado = "Ambos venceram"
+    if jogador_um == jogador_dois:
+        if jogador_um == "ataque":
+            resultado = "Aniquilacao mutua"
         elif jogador_um == "pedra":
             resultado = "Sem ganhador"
-        elif jogador_um == "ataque":
-            resultado = "Aniquilacao mutua"
-    
+        elif jogador_um == "papel":
+            resultado = "Ambos venceram"
+    elif jogador_um == "ataque":
+        resultado = "Jogador 1 venceu"
+    elif jogador_dois == "ataque":
+        resultado = "Jogador 2 venceu"
+    elif jogador_um == "pedra" and jogador_dois == "papel":
+        resultado = "Jogador 1 venceu"
+    elif jogador_dois == "pedra" and jogador_um == "papel":
+        resultado = "Jogador 2 venceu"
+
     print(resultado)
