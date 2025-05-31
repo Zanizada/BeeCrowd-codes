07dddbbea5c56cb144bbeedd895d37039f9abaf3
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-31 01:10:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2031_pedra_papel_ataque_aereo.py
+++ python/1 - Iniciante/2031_pedra_papel_ataque_aereo.py
@@ -1,5 +1,24 @@
+casos_teste = int(input())
+
 jokenpo = {
     "ataque": ["pedra", "papel"],
     "pedra": ["papel"],
-    "papel": ["papel"]
 }
+
+for i in range(casos_teste):
+    jogador_um = input()
+    jogador_dois = input()
+
+    if jogador_um in jokenpo[jogador_dois]:
+        resultado = "Jogador 2 venceu"
+    elif jogador_dois in jokenpo[jogador_um]:
+        resultado = "Jogador 1 venceu"
+    elif jogador_um == jogador_dois:
+        if jogador_um == "papel":
+            resultado = "Ambos venceram"
+        elif jogador_um == "pedra":
+            resultado = "Sem ganhador"
+        elif jogador_um == "ataque":
+            resultado = "Aniquilacao mutua"
+    
+    print(resultado)
