34f63195d71e1b838b03d35e836777cdbdde2d35
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-31 00:00:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2028_sequencia_de_sequencia.py
+++ python/1 - Iniciante/2028_sequencia_de_sequencia.py
@@ -1,9 +1,13 @@
 def sequencial(numero):
-    lista = []
+    lista = ["0"]
+    quantidade = 0
     for num in range(0, numero+1):
-        num = str(num)
-
-
+        nums = str(num)
+        numeracao = " ".join(nums * num)
+        lista.append(numeracao)
+        quantidade += num
+    del lista[1]
+    return lista, quantidade
 while True:
     try:
         numero = int(input())
