9bb7c19c894e05601a851221cbd9c4caea582d3f
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-30 23:50:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2028_sequencia_de_sequencia.py
+++ python/1 - Iniciante/2028_sequencia_de_sequencia.py
@@ -1,9 +1,8 @@
 def sequencial(numero):
     lista = []
-    for num in range(numero):
-        numeracao = f"{num}", sep=" "
-        lista.append(numeracao * num)
-    return lista
+    for num in range(0, numero+1):
+        num = str(num)
+
 
 while True:
     try:
