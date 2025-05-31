cf8e03db19d2fd519aeea1ffcdcd5c44ee38e773
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-30 23:41:02 -0300

    File modified in local repository

--- python/1 - Iniciante/2028_sequencia_de_sequencia.py
+++ python/1 - Iniciante/2028_sequencia_de_sequencia.py
@@ -1,11 +1,14 @@
 def sequencial(numero):
-    lista = [0]
+    lista = []
     for num in range(numero):
-        
+        numeracao = f"{num}", sep=" "
+        lista.append(numeracao * num)
+    return lista
 
 while True:
     try:
         numero = int(input())
+        print(sequencial(numero))
 
     except EOFError:
         break
