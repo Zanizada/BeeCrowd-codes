7e8891adb4038e7a71b1c38eb5c5cc884df93ab5
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-31 00:30:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2029_reservatorio_de_mel.py
+++ python/1 - Iniciante/2029_reservatorio_de_mel.py
@@ -1,6 +1,12 @@
+π = 3.14
+
 while True:
     try:
         volume = float(input())
         diametro = float(input())
+        raio = diametro / 2
+        altura = volume / (π * (raio ** 2))
+        area_base = π * (raio ** 2)
+    
     except EOFError:
         break
