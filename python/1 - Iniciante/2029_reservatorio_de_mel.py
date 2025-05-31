cd9a54e8090ff180c918b2442afcfdb2c74c8e2a
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-05-31 00:40:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2029_reservatorio_de_mel.py
+++ python/1 - Iniciante/2029_reservatorio_de_mel.py
@@ -1,12 +1,19 @@
 π = 3.14
+def cilindro(volume, diametro):
+    raio = diametro / 2
+    altura = volume / (π * (raio ** 2))
+    area = π * (raio ** 2)
+    altura = f"ALTURA = {altura:.2f}"
+    area = f"AREA = {area:.2f}"
+    return altura, area
 
 while True:
     try:
         volume = float(input())
         diametro = float(input())
-        raio = diametro / 2
-        altura = volume / (π * (raio ** 2))
-        area_base = π * (raio ** 2)
-    
+        altura, area = cilindro(volume, diametro)
+        print(altura)
+        print(area)
+
     except EOFError:
         break
