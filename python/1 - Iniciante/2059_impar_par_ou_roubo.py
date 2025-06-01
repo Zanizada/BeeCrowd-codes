5da35411cc8385d4c68f1498b69de0d93cec8a06
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-01 00:40:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2059_impar_par_ou_roubo.py
+++ python/1 - Iniciante/2059_impar_par_ou_roubo.py
@@ -10,5 +10,18 @@ soma = numero_um + numero_dois
 
 if roubou == 1 and acusou == 1:
     resultado = "Jogador 2 ganha!"
+elif roubou == 1 and acusou == 0:
+    resultado = "Jogador 1 ganha!"
 else:
-    
+    if soma % 2 != 0:
+        if escolha == "impar":
+            resultado = "Jogador 1 ganha!"
+        else:
+            resultado = "Jogador 2 ganha!"
+    else:
+        if escolha == "par":
+            resultado = "Jogador 1 ganha!"
+        else:
+            resultado = "Jogador 2 ganha!"
+
+print(resultado)
