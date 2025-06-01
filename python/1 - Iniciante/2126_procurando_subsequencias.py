97fbf18c66a36cfd13109396110417d8654871b0
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-01 04:40:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2126_procurando_subsequencias.py
+++ python/1 - Iniciante/2126_procurando_subsequencias.py
@@ -1,11 +1,22 @@
 while True:
     try:
+        caso = 1
         valor = int(input())
         valor = str(valor)
+        
         numero = []
         for num in valor:
             numero.append(num)
-        posicao = 0
+
+        indice = 0
+        sequencia = 0
+        subsequencia = 0
+        while indice < len(numero):
+            if numero[indice+1] == numero[indice] + 1:
+                sequencia += 1
+            if sequencia >= 1:
+                subsequencia += 1
+            indice += 1
 
     except EOFError:
         break
