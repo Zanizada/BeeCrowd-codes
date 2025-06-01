998b7661a6c23055c312ce16aabb3949afe0ade7
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-01 02:40:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2060_desafio_de_bino.py
+++ python/1 - Iniciante/2060_desafio_de_bino.py
@@ -1,13 +1,15 @@
 def multiplos(numero, multiplos=list):
-    return 2
+    contador = 0
+    resultados = []
+    for multiplo in multiplos:
+        resultado = f"{contador} Multiplos(s) de {multiplo}"
+        if numero % multiplo == 0:
+            contador += 1
+        resultados.append(resultado)
+        return resultado
 
 tamanho_lista = int(input())
 numeros = list(map(int, input().split()))
-multiplo_de_dois = 0
-multiplo_de_tres = 0
-multiplo_de_quatro = 0
-multiplo_de_cinco = 0
 
 for numero in numeros:
     if numero % 2 == 0:
-        print()
