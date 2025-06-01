09f4e4d1391f8c6babb53898e33aac3814ba4f71
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-01 04:10:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2126_procurando_subsequencias.py
+++ python/1 - Iniciante/2126_procurando_subsequencias.py
@@ -1,5 +1,11 @@
 while True:
     try:
         valor = int(input())
+        valor = str(valor)
+        numero = []
+        posicao = 0
+        for num in valor:
+            num = int(num)
+            
     except EOFError:
         break
