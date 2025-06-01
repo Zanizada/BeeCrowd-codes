b5aaacd51189967b776976cd6d305c1bb558f768
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-01 00:20:03 -0300

    File modified in local repository

--- python/1 - Iniciante/2059_impar_par_ou_roubo.py
+++ python/1 - Iniciante/2059_impar_par_ou_roubo.py
@@ -1,1 +1,11 @@
 escolha, numero_um, numero_dois, roubou, acusou = map(int, input().split())
+
+par_ou_impar = {
+    1: "par",
+    0: "impar"
+}
+
+escolha = par_ou_impar[escolha]
+
+if roubou == 1 and acusou == 1:
+    resultado = "Jogador 2 ganha!"
