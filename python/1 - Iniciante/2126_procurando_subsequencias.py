bd3100e97e263fa821635a13d3fa9eb5fa02654f
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-02 20:26:22 -0300

    File modified in local repository

--- python/1 - Iniciante/2126_procurando_subsequencias.py
+++ python/1 - Iniciante/2126_procurando_subsequencias.py
@@ -9,6 +9,7 @@ while True:
             numero.append(num)
 
         indice = 0
+        posicao = 0
         sequencia = 0
         subsequencia = 0
         while indice < len(numero):
@@ -17,6 +18,11 @@ while True:
             if sequencia >= 1:
                 subsequencia += 1
             indice += 1
-
+        
+        indice_invertido = 0
+        numero_invertido = numero[::-1]
+        while True:
+            if numero[indice_invertido] == numero[indice_invertido] - 1:
+                subsequencia
     except EOFError:
         break
