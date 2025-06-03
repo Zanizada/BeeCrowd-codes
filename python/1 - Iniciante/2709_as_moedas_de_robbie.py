b0b7af0cebad5817412e7ef855c57bf2b0901878
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-03 01:30:02 -0300

    File modified in local repository

--- python/1 - Iniciante/2709_as_moedas_de_robbie.py
+++ python/1 - Iniciante/2709_as_moedas_de_robbie.py
@@ -1,7 +1,30 @@
-qntd_moedas = int(input())
-moedas = []
-for i in range(qntd_moedas):
-    valor = int(input())
-    moedas.append(valor)
-salto = int(input())
+while True:
+    try:
+        qntd_moedas = int(input())
+        moedas = []
+        for i in range(qntd_moedas):
+            valor = int(input())
+            moedas.append(valor)
+        salto = int(input())
 
+        soma = 0
+        indice = 0
+        while indice < qntd_moedas:
+            soma += moedas[indice]
+            indice += 2
+
+        for divisor in range(2, soma):
+            if soma % divisor != 0:
+                primo = True
+            else:
+                primo = False
+
+        if primo == True:
+            resultado = "You're a coastal aircraft, Robbie, a large silver aircraft."
+        else:
+            resultado = "Bad boy! I'll hit you."
+
+        print(resultado)
+
+    except EOFError:
+        break
