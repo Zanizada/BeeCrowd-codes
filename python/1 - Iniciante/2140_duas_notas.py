8f054b1fa2bb45cf11d9800715db0f5749d6cce4
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-03 00:40:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2140_duas_notas.py
+++ python/1 - Iniciante/2140_duas_notas.py
@@ -1,1 +1,22 @@
 notas = [2, 5, 10, 20, 50, 100]
+
+while True:
+    compra, cliente = map(int, input().split())
+    if compra > cliente:
+        valor = compra - cliente
+    elif compra < cliente:
+        valor = cliente - compra
+    else:
+        break
+
+    tentados = set()
+    for nota in notas:
+        tentado = valor - nota
+        if tentado in tentados:
+            troco = "possible"
+            break
+        tentados.add(nota)
+    else:
+        troco = "impossible"
+
+    print(troco)
