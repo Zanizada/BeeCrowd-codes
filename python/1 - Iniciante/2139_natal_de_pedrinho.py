a25b21ef6c3d357bd5613571bde90f61e2c75b77
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-03 00:20:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2139_natal_de_pedrinho.py
+++ python/1 - Iniciante/2139_natal_de_pedrinho.py
@@ -6,24 +6,25 @@ while True:
             5: 31, 6: 30,
             7: 31, 8: 31,
             9: 30, 10: 31,
-            11: 30, 12: 25
+            11: 30, 12: 31
         }
 
         mes, dia = map(int, input().split())
 
-        dias = 0
-        for meses in range(1, mes+1):
-            dias += dias_por_mes[meses]
-
-        if dias == 361:
-            dias_restantes = "E natal!"
-        elif dias < 361:
-            if dias == 360:
-                dias_restantes = "E vespera de natal!"
-            else:
-                dias_restantes = f"Faltam {366 - dias} dias para o natal!"
-        else:
+        if mes == 12 and dia > 25:
             dias_restantes = "Ja passou!"
+        else:
+            dias = 0
+            for meses in range(1, mes):
+                dias += dias_por_mes[meses]
+            dias += dia
+            
+            if dias == 359:
+                dias_restantes = "E vespera de natal!"
+            elif dias == 360:
+                dias_restantes = "E natal!"
+            else:
+                dias_restantes = f"Faltam {360 - dias} dias para o natal!"
 
         print(dias_restantes)
 
