7ed332962ce803c6a00d456b4884ff118e0bd4ea
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-03 13:40:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2632_magic_and_sword.py
+++ python/1 - Iniciante/2632_magic_and_sword.py
@@ -1,1 +1,15 @@
+def raio_e_dano_da_magia(dicionario, magia, nivel):
+
 casos = int(input())
+
+magias = {
+    "fire": [200, 20, 30, 50],
+    "water": [300, 10, 25, 40],
+    "earth": [400, 25, 55, 70],
+    "air": [100, 18, 38, 60]
+}
+
+for _ in range(casos):
+    largura, altura, coord_x, coord_y = map(int, input().split())
+    magia, nivel, centro_x, centro_y = input()
+    nivel, centro_x, centro_y = int(nivel), int(centro_x), int(centro_y)
