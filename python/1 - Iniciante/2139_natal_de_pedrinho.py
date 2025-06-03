99fe598bf30e8e538e40d7a21b5b100da8e5ce8e
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-02 22:50:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2139_natal_de_pedrinho.py
+++ python/1 - Iniciante/2139_natal_de_pedrinho.py
@@ -1,4 +1,34 @@
-natal = 361
+natal = [12, 25]
+
+meses = {
+    1: "janeiro",
+    2: "fevereiro",
+    3: "março",
+    4: "abril",
+    5: "maio",
+    6: "junho",
+    7: "julho",
+    8: "agosto",
+    9: "setembro",
+    10: "outubro",
+    11: "novembro",
+    12: "dezembro"
+}
+
+dias = {
+    "janeiro": 31,
+    "fevereiro": 29,
+    "março": 31,
+    "abril": 30,
+    "maio": 31,
+    "junho": 30,
+    "julho": 31,
+    "agosto": 31,
+    "setembro": 30,
+    "outubro": 31,
+    "novembro": 30,
+    "dezembro": 31
+}
 
 while True:
     try:
