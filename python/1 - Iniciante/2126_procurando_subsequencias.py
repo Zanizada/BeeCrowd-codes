d39e489223bdd6dda4ad2665e30a75f0c9eacd43
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-02 22:10:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2126_procurando_subsequencias.py
+++ python/1 - Iniciante/2126_procurando_subsequencias.py
@@ -1,7 +1,13 @@
+import re
+
 while True:
     try:
+        caso = 1
+        casos = f"Caso #{caso}:"
         numero = int(input())
         sequencia = int(input())
-        
+        subsequencias = list(len(re.findall(f"(?={re.escape(numero)})", sequencia)))
+        posicao = subsequencias[-1].start()
+        caso += 1
     except EOFError:
         break
