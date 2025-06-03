ffd55886cd13beefb486e58a84aa335cde67555f
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-02 22:20:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2126_procurando_subsequencias.py
+++ python/1 - Iniciante/2126_procurando_subsequencias.py
@@ -1,13 +1,29 @@
 import re
 
+caso = 1
+
 while True:
     try:
-        caso = 1
-        casos = f"Caso #{caso}:"
         numero = int(input())
         sequencia = int(input())
-        subsequencias = list(len(re.findall(f"(?={re.escape(numero)})", sequencia)))
-        posicao = subsequencias[-1].start()
+        numero = str(numero)
+        sequencia = str(sequencia)
+
+        casos = f"Caso #{caso}:"
+        qtd_subsequencias = list(re.finditer(f"(?={re.escape(numero)})", sequencia))
+        
+        if qtd_subsequencias != 0:
+            qtd_subsequencias_f = f"Qtd.Subsequencias: {qtd_subsequencias}"
+            subsequencias = list(len(re.findall(f"(?={re.escape(numero)})", sequencia)))
+            posicao = f"Pos: {subsequencias[-1].start()}"
+        else:
+            qtd_subsequencias_f = f"Nao existe subsequencia"
+        
+        print(casos)
+        print(qtd_subsequencias_f)
+        print(posicao, "\n") if qtd_subsequencias != 0 else print()
+
         caso += 1
+
     except EOFError:
         break
