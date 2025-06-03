cb18355a5aef25b54d472ba307b098643df4dc3f
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-03 01:10:01 -0300

    File modified in local repository

--- python/1 - Iniciante/2152_pepe_ja_tirei_a_vela.py
+++ python/1 - Iniciante/2152_pepe_ja_tirei_a_vela.py
@@ -7,3 +7,10 @@ for i in range(casos):
         porta = "A porta abriu!"
     elif ocorrencia == 0:
         porta = "A porta fechou!"
+    
+    hora = f"{hora:02d}"
+    minuto = f"{minuto:02d}"
+    
+    tempo = ":".join([hora, minuto])
+    resultado = " - ".join([tempo, porta])
+    print(resultado)
