a484141ad31c9507cd66a765a0a9e511e5908a51
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-04 08:40:01 -0300

    File modified in local repository

--- python/1 - Iniciante/3343_attack_on_gasparini.py
+++ python/1 - Iniciante/3343_attack_on_gasparini.py
@@ -1,6 +1,7 @@
 qntd_titas, tamanho_muralhas = map(int, input().split())
 tamanho_titas = input()
 pequeno, medio, grande = map(int, input().split())
+total_muralhas = 1
 
 tamanhos = {
     "P": pequeno,
@@ -8,24 +9,4 @@ tamanhos = {
     "G": grande
 }
 
-titas = []
-for tita in tamanho_titas:
-    tita = tamanhos[tita]
-    titas.append(tita)
-
-total_tamanho_titas = sum(titas)
-total_muralhas = 1
-if tamanho_muralhas < total_tamanho_titas:
-    total_muralhas = 2
-    tamanho_muralhas += tamanho_muralhas
-    while tamanho_muralhas < total_tamanho_titas:
-        tamanho_muralhas += tamanho_muralhas
-        total_muralhas += 1
-
-print(total_muralhas)
-
-# muralha de 20 metros, 8 titãs, 5 de 10 metros, 1 de 8 metros, 2 de 3 metros:
-# 1ª muralha: 20 - 20 = 0
-# 2ª muralha: 20 - 20 = 0
-# 3ª muralha: 20 - 18 = 2
-# 4ª muralha: 20 - 6  = 4
+titas = [tamanhos[tita] for tita in tamanho_titas]
