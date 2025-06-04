c2b6a3c25c76ba598f8d3602456c70c2f034056c
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-04 09:10:01 -0300

    File modified in local repository

--- python/1 - Iniciante/3343_attack_on_gasparini.py
+++ python/1 - Iniciante/3343_attack_on_gasparini.py
@@ -11,12 +11,31 @@ tamanhos = {
 
 titas = [tamanhos[tita] for tita in tamanho_titas]
 tamanho_total_titas = sum(titas)
+muralha_atual = tamanho_muralhas
 
 if tamanho_total_titas > tamanho_muralhas:
-    for i in range(qntd_titas):
-        1 = 0
+    for i in range(1, qntd_titas+1):
+        
 
-# 3 titas, 20 metros de muralha
-# 1 médio, 1 pequeno, 1 grande
+# 8 titas, 20 metros de muralha
+# 1 médio, 2 grandes, 1 pequeno, 3 grandes, 1 pequeno
 # P = 3, M = 8, G = 10
-# 
+
+# 1ª Muralha:
+    # 20 - 8 = 12; 1 M
+    # 12 - 10 = 2; 1 G
+    # 2 < 10, CAIU.
+
+# 2ª Muralha:
+    # 20 - 10 = 10; 1 G
+    # 10 - 3  = 7;  1 P
+    # 7 < 10, CAIU.
+# 3ª Muralha:
+    # 20 - 10 = 10; 1 G
+    # 10 - 10 = 0;  1 G
+    # 0 < 10, CAIU.
+# 4ª Muralha:
+    # 20 - 10 = 10; 1 G
+    # 10 - 3  = 7;  1 P
+
+# total de muralhas: 4
