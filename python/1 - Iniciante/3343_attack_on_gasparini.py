10703b71b456f236a9097e99823e71f000dc2466
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-04 08:50:01 -0300

    File modified in local repository

--- python/1 - Iniciante/3343_attack_on_gasparini.py
+++ python/1 - Iniciante/3343_attack_on_gasparini.py
@@ -1,7 +1,6 @@
 qntd_titas, tamanho_muralhas = map(int, input().split())
 tamanho_titas = input()
 pequeno, medio, grande = map(int, input().split())
-total_muralhas = 1
 
 tamanhos = {
     "P": pequeno,
@@ -10,3 +9,14 @@ tamanhos = {
 }
 
 titas = [tamanhos[tita] for tita in tamanho_titas]
+
+total_muralhas = 0
+espaco_disponivel = 0
+
+for tita in titas:
+    if tita > espaco_disponivel:
+        total_muralhas += 1
+        espaco_disponivel = tamanho_muralhas
+    espaco_disponivel -= tita
+
+print(total_muralhas)
