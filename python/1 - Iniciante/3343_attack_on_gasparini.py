437eb511f7c6340f83c2fd6d41435b62ee63eea7
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-06-17 10:10:03 -0300

    File modified in local repository

--- python/1 - Iniciante/3343_attack_on_gasparini.py
+++ python/1 - Iniciante/3343_attack_on_gasparini.py
@@ -7,15 +7,20 @@ tamanhos = {
     "M": medio,
     "G": grande
 }
-
+# pequeno = 3, medio = 4, grande = 5
 muralhas = 1
 muralha = tamanho_muralhas
-
+# muralha = 6
 for tita in ordem_titas:
+    # tita = [G, P, M, P]
     tamanho_tita = tamanhos[tita]
-
+    # tamanho_tita = [5, 3, 4, 3]
+    muralha -= tamanho_tita
+    # 1º --> 6 - 5 = 1.
     while tamanho_tita > muralha:
+        # 5 > 1 = True:
         muralha = tamanho_muralhas
+        # 
         muralhas += 1
     
     muralha -= tamanho_tita
