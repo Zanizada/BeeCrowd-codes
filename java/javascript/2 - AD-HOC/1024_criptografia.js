450cd5481cad94fe03fe0d25c5b317342d6af76d
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-08-05 10:03:50 -0300

    File modified in local repository

--- java/javascript/2 - AD-HOC/1024_criptografia.js
+++ java/javascript/2 - AD-HOC/1024_criptografia.js
@@ -1,1 +1,13 @@
-d
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+function passada_1(texto) {
+    let caracteres = [];
+    let caractere;
+    let 
+    for (caractere in texto) {
+        if (caractere != isNaN) {
+            
+        }
+    }
+}
