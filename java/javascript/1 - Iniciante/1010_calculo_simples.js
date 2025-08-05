57743788b4bbeaf36bf7886530e2c7ea0468ddcc
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2025-08-05 09:36:24 -0300

    New item listed in local repository

--- java/javascript/1 - Iniciante/1010_calculo_simples.js
+++ java/javascript/1 - Iniciante/1010_calculo_simples.js
@@ -6,7 +6,7 @@ codigo_peca_1 = parseInt(codigo_peca_1);
 num_pecas_1 = parseInt(num_pecas_1);
 valor_pecas_1 = parseFloat(valor_pecas_1);
 
-let [codigo_peca_2, num_pecas_2, valor_pecas_2] = lines[0].split(" ");
+let [codigo_peca_2, num_pecas_2, valor_pecas_2] = lines[1].split(" ");
 codigo_peca_2 = parseInt(codigo_peca_2);
 num_pecas_2 = parseInt(num_pecas_2);
 valor_pecas_2 = parseFloat(valor_pecas_2)
